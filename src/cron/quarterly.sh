f5092960807c3cd0ad895da60a90ba119f5d2490
David Scudiero <dscudiero@leepfrog.com>
2016-12-29 16:50:40 -0600

    Updated the code to escrow sites to generalize

--- src/cron/quarterly.sh
+++ src/cron/quarterly.sh
@@ -1,7 +1,7 @@
 #=======================================================================================================================
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version=2.1.9 # -- dscudiero -- 12/14/2016 @ 11:34:20.66
+version=2.1.12 # -- dscudiero -- 12/29/2016 @ 16:46:55.10
 #=======================================================================================================================
 # Run every day at noon from cron
 #=======================================================================================================================
@@ -12,7 +12,24 @@ originalArgStr="$*"
 #=======================================================================================================================
 # Declare local variables and constants
 #=======================================================================================================================
-
+function EscrowSite {
+	local clientList="$*"
+	Msg2 > $tmpFile
+	Msg2 "$(date)">> $tmpFile
+	Msg2 >> $tmpFile
+	Msg2 "The following sites have been escrowed, the escrow files can be found at \n^'$courseleafEscrowedSitesDir'" >> $tmpFile
+	for client in $(tr ',' ' ' <<< $build7EscrowClients) do
+		Msg2 "^$client"
+		Call 'escrowClient' "$client" "$scriptArgs"
+	done
+	Msg2 >> $tmpFile
+	if [[ $sendMail == true ]]; then
+		Msg2 "\nEmails sent to: $escrowEmailAddrs\n" >> $tmpFile
+		for emailAddr in $(tr ',' ' ' <<< $escrowEmailAddrs); do
+			mail -s "$myName: Clients escrowed" $emailAddrs < $tmpFile
+		done
+	fi
+}
 #=======================================================================================================================
 # Standard argument parsing and initialization
 #=======================================================================================================================
@@ -27,28 +44,13 @@ sendMail=true
 #==================================================================================================
 case "$hostName" in
 	mojave)
-			## Escrow sites
-				Msg2 > $tmpFile
-				Msg2 "$myName $(date)\n" >> $tmpFile
-				Msg2 > $tmpFile
-
-				for client in $(tr ',' ' ' <<< $escrowClients); do
-					Call 'escrowClient' "$client" "$scriptArgs"
-					Msg2 "^$client" >> $tmpFile
-				done
-				Msg2 >> $tmpFile
-
-				if [[ $sendMail == true ]]; then
-					Msg2 "\nEmails sent to: $escrowEmailAddrs\n" >> $tmpFile
-					for emailAddr in $(tr ',' ' ' <<< $escrowEmailAddrs); do
-						mail -s "$myName: Clients escrowed" $emailAddrs < $tmpFile
-					done
-				fi
-				[[ -f $tmpFile ]] && rm -f $tmpFile
+			[[ -n $mojaveEscrowClients ]] && EscrowSite "$mojaveEscrowClients"
 			;;
 	build5)
+			[[ -n $build5EscrowClients ]] && EscrowSite "$build5EscrowClients"
 			;;
 	build7)
+			[[ -n $build7EscrowClients ]] && EscrowSite "$build7EscrowClients"
 			;;
 esac
 
@@ -60,3 +62,4 @@ return 0
 #========================================================================================================================
 # Change Log
 #========================================================================================================================
+## Thu Dec 29 16:50:40 CST 2016 - dscudiero - Updated the code to escrow sites to generalize
