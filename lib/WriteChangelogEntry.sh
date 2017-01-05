af1418aff13130b78ad93fb48798833677ed6458
David Scudiero <dscudiero@leepfrog.com>
2017-01-05 07:58:50 -0600

    Refactored to correctly write data out to the appropriate file

--- lib/WriteChangelogEntry.sh
+++ lib/WriteChangelogEntry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.6" # -- dscudiero -- 01/04/2017 @ 13:51:23.26
+# version="2.0.7" # -- dscudiero -- 01/05/2017 @  7:58:29.88
 #===================================================================================================
 # Write a 'standard' format courseleaf changelog.txt
 # args: "logFileName" ${lineArray[@]}
@@ -23,23 +23,20 @@ function WriteChangelogEntry {
 
 	local clientDataLogFile
 
-	## If there is a clientData folder then writout there also
-		if [[ -d $localClientWorkFolder ]]; then
+	## If there is a clientData folder then write out to there also
+		if [[ -n $client && -d $localClientWorkFolder ]]; then
 			[[ ! -d $localClientWorkFolder/$client ]] && mkdir -p $localClientWorkFolder/$client
 			clientDataLogFile="$localClientWorkFolder/$client/changelog.txt"
-		else
-			clientDataLogFile="/dev/null"
 		fi
 
 	## Write out records
-		printf "\n$userName\t$(date) via '$logger' version: $version\n" >> "$logFile"
-		printf "\n$userName\t$(date) via '$logger' version: $version\n" >> "$clientDataLogFile"
+		echo -e "\n$userName\t$(date) via '$logger' version: $version" >> "$logFile"
+		[[ -n $clientDataLogFile ]] && echo -e "\n$userName\t$(date) via '$logger' version: $version" >> "$clientDataLogFile"
+		[[ -n $clientDataLogFile && -n $env ]] &&  echo -e '\t%s\n' "Environment: ${env}${tgtEnv}" >> "$clientDataLogFile"
 
-		printf '\t%s\n' "Environment: ${env}${tgtEnv}" >> "$clientDataLogFile"
-
-		if [[ $ref != '' ]]; then
-			printf '\t%s\n' "${!ref}" >> "$logFile" >> "$logFile"
-			printf '\t%s\n' "${!ref}" >> "$logFile" >> "$clientDataLogFile"
+		if [[ -n $ref ]]; then
+			printf '\t%s\n' "${!ref}" >> "$logFile"
+			[[ -n $clientDataLogFile ]] && printf '\t%s\n' "${!ref}" >> "$clientDataLogFile"
 		fi
 
 	return 0
@@ -50,3 +47,4 @@ export -f WriteChangelogEntry
 ## Check-in log
 #===================================================================================================
 ## Wed Jan  4 13:54:41 CST 2017 - dscudiero - General syncing of dev to prod
+## Thu Jan  5 07:58:49 CST 2017 - dscudiero - Refactored to correctly write data out to the appropriate file
