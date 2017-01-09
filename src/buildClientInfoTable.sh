48909aa0b84ed636a79e26bd736acda6fdf5575b
David Scudiero <dscudiero@leepfrog.com>
2017-01-09 13:29:01 -0600

    Add an inplace option to just update the clients table directly

--- src/buildClientInfoTable.sh
+++ src/buildClientInfoTable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version=2.3.86 # -- dscudiero -- 01/06/2017 @  7:41:08.67
+version=2.3.99 # -- dscudiero -- 01/09/2017 @ 10:56:29.19
 #=======================================================================================================================
 TrapSigs 'on'
 imports='GetDefaultsData ParseArgs ParseArgsStd Hello Init Goodbye' #imports="$imports "
@@ -17,6 +17,16 @@ scriptDescription="Sync client warehouse and transactional tables"
 # 06-17-15 -- 	dgs - Initial coding
 # 07-17-15 --	dgs - Migrated to framework 5
 #=======================================================================================================================
+
+#=======================================================================================================================
+# Standard call back functions
+#=======================================================================================================================
+function parseArgs-buildClientInfoTable  { # or parseArgs-local
+	#argList+=(-optionArg,1,option,scriptVar,,script,'Help text')
+	#argList+=(-flagArg,2,switch,scriptVar,,script,'Help text')
+	argList+=(-inPlace,4,switch,inPlace,,script,'Load the main clients table')
+	return 0
+}
 
 #=======================================================================================================================
 # Declare local variables and constants
@@ -52,7 +62,8 @@ fi
 #=======================================================================================================================
 ParseArgsStd
 Hello
-useClientInfoTable="${clientInfoTable}New"
+
+[[ $inPlace == true ]] && useClientInfoTable="$clientInfoTable" || useClientInfoTable="${clientInfoTable}New"
 Msg2 "Database: $warehouseDb"
 Msg2 "Table: $useClientInfoTable"
 
@@ -76,12 +87,17 @@ Msg2 "Table: $useClientInfoTable"
 	fi
 
 ## Create a temporary copy of the clients table, load new data to that table
-	sqlStmt="drop table if exists ${clientInfoTable}Bak"
-	RunSql2 $sqlStmt
-	sqlStmt="drop table if exists $useClientInfoTable"
-	RunSql2 $sqlStmt
-	sqlStmt="create table $useClientInfoTable like ${clientInfoTable}"
-	RunSql2 $sqlStmt
+	if [[ $inPlace == true ]]; then
+		sqlStmt="truncate $useClientInfoTable"
+		RunSql2 $sqlStmt
+	else
+		sqlStmt="drop table if exists ${clientInfoTable}Bak"
+		RunSql2 $sqlStmt
+		sqlStmt="drop table if exists $useClientInfoTable"
+		RunSql2 $sqlStmt
+		sqlStmt="create table $useClientInfoTable like ${clientInfoTable}"
+		RunSql2 $sqlStmt
+	fi
 
 ## Loop through clients
 	clientCntr=0
@@ -116,17 +132,19 @@ Msg2 "Table: $useClientInfoTable"
 	fi
 
 ## Swap client tables
-	[[ $batchMode != true ]] && Msg2 "^Swapping databases ..."
-	sqlStmt="select count{*) from ${clientInfoTable}New"
-	RunSql2
-	[[ ${#resultSet[@]} -eq 0 ]] &&  Terminate "New clients table has zero rows, keeping original"
+	if [[ $inPlace != true ]]; then
+		[[ $batchMode != true ]] && Msg2 "^Swapping databases ..."
+		sqlStmt="select count{*) from ${clientInfoTable}New"
+		RunSql2
+		[[ ${#resultSet[@]} -eq 0 ]] &&  Terminate "New clients table has zero rows, keeping original"
 
-	sqlStmt="rename table $clientInfoTable to ${clientInfoTable}Bak"
-	RunSql2 $sqlStmt
-	[[ $batchMode != true ]] && Msg2 "^^$clientInfoTable --> ${clientInfoTable}Bak"
-	sqlStmt="rename table ${clientInfoTable}New to $clientInfoTable"
-	RunSql2 $sqlStmt
-	[[ $batchMode != true ]] && Msg2 "^^${clientInfoTable}New --> $clientInfoTable"
+		[[ $batchMode != true ]] && Msg2 "^^$clientInfoTable --> ${clientInfoTable}Bak"
+		sqlStmt="rename table $clientInfoTable to ${clientInfoTable}Bak"
+		RunSql2 $sqlStmt
+		[[ $batchMode != true ]] && Msg2 "^^${clientInfoTable}New --> $clientInfoTable"
+		sqlStmt="rename table ${clientInfoTable}New to $clientInfoTable"
+		RunSql2 $sqlStmt
+	fi
 
 #=======================================================================================================================
 # Done
@@ -139,10 +157,11 @@ Goodbye 0 'alert'
 ## Mon Jun  6 13:11:14 CDT 2016 - dscudiero - Renamed and Re-factored
 ## Mon Jun  6 13:27:36 CDT 2016 - dscudiero - General syncing of dev to prod
 ## Tue Jul 12 07:10:42 CDT 2016 - dscudiero - Add override parameters to callPgm
-## Thu Jan  5 12:37:46 CST 2017 - dscudiero - refactored to create a new table, insert data and then rename if successful
+## Thu Jan  5 12:37:46 CST 2017 - dscudiero - re-factored to create a new table, insert data and then rename if successful
 ## Thu Jan  5 12:53:22 CST 2017 - dscudiero - remove debug statements
 ## Thu Jan  5 12:56:56 CST 2017 - dscudiero - General syncing of dev to prod
 ## Thu Jan  5 13:40:11 CST 2017 - dscudiero - removed debug code
 ## Thu Jan  5 14:00:07 CST 2017 - dscudiero - General syncing of dev to prod
 ## Fri Jan  6 07:26:18 CST 2017 - dscudiero - Fix syntax error
 ## Fri Jan  6 07:41:30 CST 2017 - dscudiero - Add Messages for the swap db process
+## Mon Jan  9 13:29:00 CST 2017 - dscudiero - Add an inplace option to just update the clients table directly
