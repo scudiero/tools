b838f73e3b65e75ced5de28c0683bd13de845513
David Scudiero <dscudiero@leepfrog.com>
2017-01-11 07:51:24 -0600

    Streamlined

--- lib/ProcessLogger.sh
+++ lib/ProcessLogger.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="1.0.10" # -- dscudiero -- 01/06/2017 @ 16:38:16.08
+# version="1.0.17" # -- dscudiero -- 01/11/2017 @  7:47:41.16
 #===================================================================================================
 # Write out / update a start record into the process log database
 #===================================================================================================
@@ -10,10 +10,10 @@
 
 function ProcessLogger {
 	[[ $logInDb == false ]] && return 0
-	local mode=$(Lower ${1:0:1}); shift || true
+	local mode="$1"; shift || true
 	local myName argString epochTime sqlStmt epochTime epochStime epochEtime eHr eMin eSec elapSeconds elapTime
 
-	case $mode in
+	case $(Lower ${mode:0:1}) in
 	    s)	# START
 			myName=$1; shift || true
 			argString="$*"
@@ -23,46 +23,35 @@ function ProcessLogger {
 			sqlStmt="insert into $processLogTable (idx,name,hostName,userName,viaScripts,startTime,startEtime,endTime,endEtime,elapsedTime,exitCode,argString,data) \
 					values(NULL,\"$myName\",\"$hostName\",\"$userName\",\"$calledViaScripts\",\"$startTime\",$(date +%s),NULL,NULL,NULL,NULL,\"$argString\",NULL)"
 			RunSql2 $sqlStmt
-			## Get the inserted record id
-			sqlStmt="select max(idx) from $processLogTable"
-			RunSql2 $sqlStmt
+			## Get newly inserted record Id
+			[[ ${#resultSet[@]} -eq 0 ]] && Terminate "Could not insert record into $processLogTable"
 			echo ${resultSet[0]}
 			;;
 	    e)	# END
 			idx=$1
 			[[ -z $idx ]] && return 0
 			sqlStmt="select startEtime from $processLogTable where idx=$idx"
+			RunSql2 $sqlStmt
 			epochStime=${resultSet[0]}
 			epochEtime=$(date +%s)
 			endTime=$(date '+%Y-%m-%d %H:%M:%S')
-			elapSeconds=$(( epochEtime - epochStime ))
-			eHr=$(( elapSeconds / 3600 ))
-			elapSeconds=$(( elapSeconds - eHr * 3600 ))
-			eMin=$(( elapSeconds / 60 ))
-			elapSeconds=$(( elapSeconds - eMin * 60 ))
-			eSec=$elapSeconds
-			elapTime=$(printf "%02dh %02dm %02ds" $eHr $eMin $eSec)
+			if [[ -z $epochStime ]]; then
+				elapTime="N/A"
+			else
+				elapSeconds=$(( epochEtime - epochStime ))
+				eHr=$(( elapSeconds / 3600 ))
+				elapSeconds=$(( elapSeconds - eHr * 3600 ))
+				eMin=$(( elapSeconds / 60 ))
+				elapSeconds=$(( elapSeconds - eMin * 60 ))
+				eSec=$elapSeconds
+				elapTime=$(printf "%02dh %02dm %02ds" $eHr $eMin $eSec)
+			fi
 			sqlStmt="update $processLogTable set endTime=\"$endTime\",endEtime=\"$epochEtime\",elapsedTime=\"$elapTime\" where idx=$idx"
-			RunSql2 $sqlStmt
-	        ;;
-	    x)	# SET EXIT CODE
-			idx=$1; shift || true
-			[[ -z $idx ]] && return 0
-			argString="$*"
-			sqlStmt="update $processLogTable set exitCode=\"$argString\" where idx=$idx"
-			RunSql2 $sqlStmt
-	        ;;
-	    d)	# UPDATE DATA FIELD
-			idx=$1; shift || true
-			[[ -z $idx ]] && return 0
-			argString="$*"
-			[[ $informationOnlyMode == true ]] && argString="${argString}, informationOnlyMode"
-			[[ $allItems != '' && $(Contains "$argString" 'allItems') != true ]] && argString="${argString}, AllItems"
-			sqlStmt="update $processLogTable set data=\"$argString\" where idx=$idx"
 			RunSql2 $sqlStmt
 	        ;;
 	    u)	# UPDATE DATA FIELD, The second token is the name of the field to update
 			idx=$1; shift || true
+			[[ -z $idx ]] && return 0
 			local field=$1; shift || true
 			argString="$*"
 			sqlStmt="update $processLogTable set $field=\"$argString\" where idx=$idx"
@@ -84,3 +73,4 @@ export -f ProcessLogger
 #===================================================================================================
 # Check-in Log
 #===================================================================================================
+## Wed Jan 11 07:51:24 CST 2017 - dscudiero - Streamlined
