d3ea224172695c24568878a067ec9f0b0922b09c
David Scudiero <dscudiero@leepfrog.com>
2017-02-08 10:57:38 -0600

    General syncing of dev to prod

--- lib/MkTmpFile.sh
+++ lib/MkTmpFile.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.7" # -- dscudiero -- 01/17/2017 @  8:24:16.04
+# version="2.0.8" # -- dscudiero -- 02/07/2017 @ 17:03:37.80
 #===================================================================================================
 # Get a temp file name
 #===================================================================================================
@@ -9,11 +9,13 @@
 #===================================================================================================
 
 function MkTmpFile {
-	local functionName=${1:-$FUNCNAME}
 	[[ -z $tmpRoot ]] && tmpRoot=/tmp/$LOGNAME
 	[[ ! -d $tmpRoot ]] && mkdir -p $tmpRoot
-	local tmpFile="$(mktemp $tmpRoot/$myName.$functionName.XXXXXXXXXX)"
-	echo "$tmpFile"
+	if [[ -z "$1" ]]; then
+		echo "$(mktemp $tmpRoot/$myName.XXXXXXXXXX)"
+	else
+		echo "$(mktemp $tmpRoot/$myName.$1.XXXXXXXXXX)"
+	fi
 	return 0
 }
 function mkTmpFile { MkTmpFile "$*" ; }
@@ -25,3 +27,4 @@ export -f mkTmpFile
 #===================================================================================================
 ## Wed Jan  4 13:54:00 CST 2017 - dscudiero - General syncing of dev to prod
 ## Tue Jan 17 08:57:48 CST 2017 - dscudiero - Make sure there is a value for tmpRoot id defaults were not loaded
+## Wed Feb  8 10:57:38 CST 2017 - dscudiero - General syncing of dev to prod
