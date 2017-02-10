db7ceba10bd931dfb6a544e17f44c775d9c7c9ff
David Scudiero <dscudiero@leepfrog.com>
2017-02-10 14:58:55 -0600

    add 2 day summary reports back

--- src/cron/weekly.sh
+++ src/cron/weekly.sh
@@ -1,7 +1,7 @@
 #=======================================================================================================================
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version=2.1.19 # -- dscudiero -- 01/17/2017 @  7:24:14.99
+version=2.1.20 # -- dscudiero -- 02/10/2017 @ 14:58:31.84
 #=======================================================================================================================
 # Run every day at noon from cron
 #=======================================================================================================================
@@ -29,8 +29,8 @@ case "$hostName" in
 			Msg2 "\n Publishing Report..."
 			Call 'reports' "publishing -email 'froggersupport@leepfrog.com' $scriptArgs"
 
-			#Msg2 "\n Client 2 Day Summaries Report..."
-			#Call 'reports' "client2DaySummaries -role 'support' -email 'dscudiero@leepfrog.com,jlindeman@leepfrog.com' $scriptArg"
+			Msg2 "\n Client 2 Day Summaries Report..."
+			Call 'reports' "client2DaySummaries -role 'support' -email 'dscudiero@leepfrog.com' $scriptArg"
 
 			Msg2 "\n Tools Usage Report..."
 			Call 'reports' "toolsUsage -email 'dscudiero@leepfrog.com' $scriptArgs"
@@ -56,3 +56,4 @@ return 0
 #========================================================================================================================
 ## Thu Jan  5 14:50:18 CST 2017 - dscudiero - Switch to use RunSql2
 ## Tue Jan 17 07:42:31 CST 2017 - dscudiero - comment out some reports
+## Fri Feb 10 14:58:55 CST 2017 - dscudiero - add 2 day summary reports back
