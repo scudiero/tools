574ad3747b62f59a39685623ea331f2ce569d53b
David Scudiero <dscudiero@leepfrog.com>
2017-03-14 10:36:01 -0500

    Add prefix argument to controle tabbing

--- lib/GetCims.sh
+++ lib/GetCims.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.3" # -- dscudiero -- 01/05/2017 @ 15:29:48.69
+# version="2.0.7" # -- dscudiero -- 03/14/2017 @ 10:29:37.95
 #===================================================================================================
 # Get CIMs
 #===================================================================================================
@@ -9,7 +9,9 @@
 #===================================================================================================
 function GetCims {
 	Msg2 $V3 "*** $FUNCNAME -- Starting ***"
-	local siteDir=$1 ans suffix validVals
+	local siteDir=$1 ; shift || true
+	local prefix="$1"; #[[ -z $prefix ]] && prefix="\t"
+	local ans suffix validVals
 	if [[ $allowMultiCims == true ]]; then
 		suffix=', a for all cims'
 		validVals='Yes No All'
@@ -30,7 +32,7 @@ function GetCims {
 			[[ $onlyCimsWithTestFile == true && ! -f $siteDir/web/$dir/wfTest.xml ]] && continue
 			if [[ $verify == true && $allCims != true ]]; then
 				unset ans
-				Prompt ans "\tFound CIM Instance '$(ColorK $dir)' in source instance,\n\t\tdo you wish to use it? (y to use$suffix)? >"\
+				Prompt ans "${prefix}Found CIM Instance '$(ColorK $dir)' in source instance,\n${prefix}\tdo you wish to use it? (y to use$suffix)? >"\
 			 			"$validVals"; ans=$(Lower ${ans:0:1});
 				[[ $ans == 'a' ]] && cims=(${adminDirs[@]}) && break
 				if [[ $ans == 'y' ]]; then
@@ -60,3 +62,4 @@ export -f GetCims
 
 ## Wed Jan  4 13:53:29 CST 2017 - dscudiero - General syncing of dev to prod
 ## Thu Jan  5 15:30:07 CST 2017 - dscudiero - modify debug messages
+## Tue Mar 14 10:36:01 CDT 2017 - dscudiero - Add prefix argument to controle tabbing
