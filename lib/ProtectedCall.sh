f035427409d10c75313d3cdfa09ca8182c218236
David Scudiero <dscudiero@leepfrog.com>
2017-05-05 13:21:26 -0500

    Remove GD code

--- lib/ProtectedCall.sh
+++ lib/ProtectedCall.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.8" # -- dscudiero -- 01/11/2017 @ 15:38:06.80
+# version="2.0.9" # -- dscudiero -- Fri 05/05/2017 @ 13:19:55.05
 #===================================================================================================
 # Run a command and ignore non zero exit code trapping
 #===================================================================================================
@@ -9,7 +9,6 @@
 #===================================================================================================
 
 function ProtectedCall {
-	#GD echo "In $FUNCNAME \$\* = >"$*'<'
 	previousTrapERR=$(trap -p ERR | cut -d ' ' -f3-)
 	trap - ERR
 	[[ $verbose == true && $verboseLevel -gt 1 ]] && printf "\n$FUNCNAME - $(date)\n" >> $stdout && printf "\tcwd: $(pwd)\n" >> $stdout && printf "\t$*\n\n" >> $stdout
@@ -29,3 +28,4 @@ export -f ProtectedCall
 
 ## Wed Jan  4 13:54:11 CST 2017 - dscudiero - General syncing of dev to prod
 ## Wed Jan 11 15:38:48 CST 2017 - dscudiero - switch format of some if statementst to se -n
+## 05-05-2017 @ 13.21.26 - ("2.0.9")   - dscudiero - Remove GD code
