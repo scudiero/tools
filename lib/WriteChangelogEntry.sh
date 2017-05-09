ac4775093bebf0783e22fcb6e8de3ee6db4a5fa6
David Scudiero <dscudiero@leepfrog.com>
2017-05-09 11:55:52 -0500

    Refactored how logging is done, added an user activity log file

--- lib/WriteChangelogEntry.sh
+++ lib/WriteChangelogEntry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.11" # -- dscudiero -- 01/24/2017 @ 12:44:18.34
+# version="2.0.12" # -- dscudiero -- Tue 05/09/2017 @ 11:55:14.69
 #===================================================================================================
 # Write a 'standard' format courseleaf changelog.txt
 # args: "logFileName" ${lineArray[@]}
@@ -15,29 +15,27 @@
 # args: "logFileName" ${lineArray[@]}
 #===================================================================================================
 function WriteChangelogEntry {
-	[[ $DOIT != '' || $listOnly == true || $informationOnlyMode == true ]] && return 0
 	local ref=$1[@]
+	[[ -z $ref || -n $DOIT || $listOnly == true || $informationOnlyMode == true ]] && return 0
 	local logFile="$2"
 	local logger=${3-$myName}
 	[[ ! -f "$logFile" ]] && touch "$logFile"
 
-	local clientDataLogFile
+	local clientDataLogFile clientSummaryLogFile
+	local usersClientLogFile="/dev/null"
+	local usersActivityLog="/dev/null"
 
 	## If there is a clientData folder then write out to there also
-		if [[ -n $client && -d $localClientWorkFolder ]]; then
-			[[ ! -d $localClientWorkFolder/$client ]] && mkdir -p $localClientWorkFolder/$client
-			clientDataLogFile="$localClientWorkFolder/$client/changelog.txt"
+		if [[ -n $localClientWorkFolder && -d $localClientWorkFolder ]]; then
+			[[ -n $client && ! -d "$localClientWorkFolder/$client" ]] && mkdir -p $localClientWorkFolder/$client
+			usersClientLogFile="$localClientWorkFolder/$client/changelog.txt"
+			usersActivityLog="$localClientWorkFolder/activityLog.txt"
 		fi
 
 	## Write out records
-		echo -e "\n$userName\t$(date) via '$logger' version: $version" >> "$logFile"
-		[[ -n $clientDataLogFile ]] && echo -e "\n$userName\t$(date) via '$logger' version: $version" >> "$clientDataLogFile"
-		[[ -n $clientDataLogFile && -n $env ]] &&  echo -e '\t\n' "Environment: ${env}${tgtEnv}" >> "$clientDataLogFile"
-
-		if [[ -n $ref ]]; then
-			printf '\t%s\n' "${!ref}" >> "$logFile"
-			[[ -n $clientDataLogFile ]] && printf '\t%s\n' "${!ref}" >> "$clientDataLogFile"
-		fi
+		echo -e "\n$userName\t$(date) via '$logger' version: $version" | tee -a "$logFile" | tee -a "$usersActivityLog" >> "$usersClientLogFile"
+		[[ -n $env ]] && echo -e "\tEnvironment: $env" | tee -a "$usersActivityLog" >> "$usersClientLogFile"
+		printf '\t%s\n' "${!ref}" | tee -a "$logFile" | tee -a "$usersActivityLog" >> "$usersClientLogFile"
 
 	return 0
 }
@@ -50,3 +48,4 @@ export -f WriteChangelogEntry
 ## Thu Jan  5 07:58:49 CST 2017 - dscudiero - Refactored to correctly write data out to the appropriate file
 ## Thu Jan 19 12:49:07 CST 2017 - dscudiero - x
 ## Tue Jan 24 12:48:10 CST 2017 - dscudiero - Fix errant '%' in the output
+## 05-09-2017 @ 11.55.51 - ("2.0.12")  - dscudiero - Refactored how logging is done, added an user activity log file
