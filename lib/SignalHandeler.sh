89dc06283e7a86d7bf6775da3ffccb3a9fc67c33
David Scudiero <dscudiero@leepfrog.com>
2017-05-10 09:45:49 -0500

    General syncing of dev to prod

--- lib/SignalHandeler.sh
+++ lib/SignalHandeler.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.57" # -- dscudiero -- Fri 04/14/2017 @ 12:16:28.56
+# version="2.0.65" # -- dscudiero -- Wed 05/10/2017 @  9:41:37.11
 #===================================================================================================
 # Process interrupts
 #===================================================================================================
@@ -9,18 +9,17 @@
 #===================================================================================================
 
 function SignalHandeler {
-    Import 'Goodbye'
+    #Import 'Goodbye'
     VerboseMsg 3 "*** Starting: $FUNCNAME ***"
 	local sig="$(Upper $1)"
     local errorLineNo="$2"
-    local errorCode="$3"
+    local errorCode="${3-3}"
     parentModule="$(echo $(caller) | cut -d' ' -f2)"
     local errorLine="$(Trim "$(sed "$errorLineNo!d" "$parentModule")")"
     parentModule=$(basename $parentModule)
-    #dump -p sig errorLineNo errorCode parentModule errorLine
-    #printf '%s\n' "${BASH_SOURCE[@]}"
     local message
 
+    dump -1 sig
     case "$sig" in
         ERR)
             message="$FUNCNAME: Unknown error condition ($errorCode) raised in module '$parentModule', \n^$(ColorE "line($errorLineNo)"): '$(ColorK "$errorLine")'"
@@ -33,23 +32,21 @@ function SignalHandeler {
             ;;
         *)
             message="$FUNCNAME: Trapped signal: '$sig' in module\n^'$parentModule'"
-            ;;
     esac
 
-    ## Quit
-        if [[ $message != '' && $errorCode != '255' ]]; then
-            echo -e "\n$(PadChar)"
-            Error "$message";
-            Msg2 "\n^Call Stack:"
-            local IFSsave="$IFS" module callStack
-            callStack="$(GetCallStack '|')"
-            IFS='|' ; for module in $callStack; do
-                Msg2 "^^$module"
-            done ; IFS="$IFSsave"
-            echo -e "$(PadChar)\n"
-        fi
-        trap - EXIT
-        Goodbye 3
+    if [[ -n $message && $errorCode != '255' ]]; then
+        echo -e "\n$(PadChar)"
+        Error "$message";
+        Msg2 "\n^Call Stack:"
+        local IFSsave="$IFS" module callStack
+        callStack="$(GetCallStack '|')"
+        IFS='|' ; for module in $callStack; do
+            Msg2 "^^$module"
+        done ; IFS="$IFSsave"
+        echo -e "$(PadChar)\n"
+    fi
+    trap - EXIT
+    Goodbye $errorCode
 
 } #Signal_handler
 export -f SignalHandeler
@@ -62,3 +59,4 @@ export -f SignalHandeler
 ## Mon Feb  6 09:41:06 CST 2017 - dscudiero - Tweak messaging
 ## 04-14-2017 @ 12.04.05 - ("2.0.56")  - dscudiero - refactor how the call path is displayed
 ## 04-14-2017 @ 12.18.05 - ("2.0.57")  - dscudiero - Send a bad condition code to Goodbye
+## 05-10-2017 @ 09.45.48 - ("2.0.65")  - dscudiero - General syncing of dev to prod
