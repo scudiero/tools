bc96896cfe7c7998a8794f4dc14fff26014381e0
David Scudiero <dscudiero@leepfrog.com>
2017-05-16 06:45:47 -0500

    If the first token in the argument list is 'cronjob.sh' then strip it off

--- src/reports.sh
+++ src/reports.sh
@@ -1,9 +1,11 @@
 ##  #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.1 # -- dscudiero -- Fri 05/12/2017 @ 13:41:39.82
+version=1.0.2 # -- dscudiero -- Tue 05/16/2017 @  6:45:13.15
 #==================================================================================================
 # Quick call to scriptsAndReports
 #==================================================================================================
+[[ $1 == 'cronjob.sh' ]] && shift
 Call scriptsAndReports $(basename $0) $*
 ## 05-12-2017 @ 13.42.46 - (1.0.1)     - dscudiero - General syncing of dev to prod
+## 05-16-2017 @ 06.45.47 - (1.0.2)     - dscudiero - If the first token in the argument list is 'cronjob.sh' then strip it off
