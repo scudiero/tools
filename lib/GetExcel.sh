6efd681faf9210e43f6418447d3ef85e4f00809f
David Scudiero <dscudiero@leepfrog.com>
2017-05-18 12:13:01 -0500

    General syncing of dev to prod

--- lib/GetExcel.sh
+++ lib/GetExcel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## XO NOT AUTOVERSION
 #=======================================================================================================================
-# version="2.0.64" # -- dscudiero -- Wed 05/17/2017 @ 12:26:24.62
+# version="2.0.66" # -- dscudiero -- Thu 05/18/2017 @ 12:12:29.26
 #=======================================================================================================================
 # Retrieve data from a Excel xlsx spreadsheet
 # Usage: GetExcel <workBook> <workSheet>
@@ -15,30 +15,32 @@ function GetExcel {
 		local workSheet="$1"; shift
 		local delimiter=${1-|}
 		local tmpFile=$(MkTmpFile $FUNCNAME)
-		verboseLevelSave=$verboseLevel
-		verboseLevel=0
-		Call 'getXlsx' 'utility' 'std' 'python:py' "$workBook" "$workSheet" "$delimiter" > $tmpFile 2>&1;
-		verboseLevel=$verboseLevelSave
+		function GetExcelCleanup { SetFileExpansion 'on' ; rm -rf ${tmpFile}* >& /dev/null ; SetFileExpansion ; }
 
+		[[ ! -f $workBook ]] && Terminate "$FUNCNAME: Could not locate workbook file '$workBook'"
+		if [[ $(Contains "$workBook" ' ') == true ]]; then
+			cp -fp "$workBook" "$tmpFile.workbook"
+			workBook="$tmpFile.workbook"
+		fi
+
+		verboseLevelSave=$verboseLevel ; verboseLevel=0
+		Call 'getXlsx' 'utility' 'std' 'python:py' "$workBook" "$workSheet" "$delimiter" -vv > $tmpFile 2>&1;
+		verboseLevel=$verboseLevelSave
 		local grepStr=$(ProtectedCall "grep '*Fatal Error*' $tmpFile")
 		[[ $grepStr == '' ]] && grepStr=$(ProtectedCall "grep '*Error*' $tmpFile")
 		if [[ $grepStr != '' || $(tail -n 1 $tmpFile) == '-1' ]]; then
 			Msg2 $E "Could not retrieve data from workbook, please see below"
 			tail -n 10 $tmpFile > $tmpFile.2
 			while read -r line; do echo -e "\t$line"; done < $tmpFile.2;
-			[[ -f $tmpFile.2 ]] && rm -f $tmpFile.2
-			[[ -f $tmpFile ]] && rm -f $tmpFile
+			GetExcelCleanup
 			Msg2
 			Goodbye -1
 		fi
 		cat $tmpFile
-
-		[[ -f $tmpFile.2 ]] && rm -f $tmpFile.2
-		[[ -f $tmpFile ]] && rm -f $tmpFile
+		GetExcelCleanup
 
 	return 0
 } #GetExcel
-export -f GetExcel
 
 #=======================================================================================================================
 # Check-in Log
@@ -50,3 +52,4 @@ export -f GetExcel
 ## 04-12-2017 @ 13.25.17 - ("2.0.62")  - dscudiero - x
 ## 04-12-2017 @ 15.36.02 - ("2.0.63")  - dscudiero - remove debug statements
 ## 05-17-2017 @ 12.26.49 - ("2.0.64")  - dscudiero - Turn off messages when running the python procedure
+## 05-18-2017 @ 12.13.01 - ("2.0.66")  - dscudiero - General syncing of dev to prod
