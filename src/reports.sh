50013ec080e35ba8341dfbaeda29f937c16bc55e
David Scudiero <dscudiero@leepfrog.com>
2017-05-19 14:22:32 -0500

    Change call name to an absolute 'reports'

--- src/reports.sh
+++ src/reports.sh
@@ -1,11 +1,11 @@
 ##  #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.4 # -- dscudiero -- Fri 05/19/2017 @  7:26:22.78
+version=1.0.8 # -- dscudiero -- Fri 05/19/2017 @ 14:22:02.14
 #==================================================================================================
 # Quick call to scriptsAndReports
 #==================================================================================================
-Call scriptsAndReports $(basename $0) $*
+Call scriptsAndReports 'reports' $*
 
 #==================================================================================================
 # Check-in Log
@@ -14,3 +14,4 @@ Call scriptsAndReports $(basename $0) $*
 ## 05-16-2017 @ 06.45.47 - (1.0.2)     - dscudiero - If the first token in the argument list is 'cronjob.sh' then strip it off
 ## 05-18-2017 @ 06.58.15 - (1.0.2)     - dscudiero - add debug messages
 ## 05-19-2017 @ 07.26.31 - (1.0.4)     - dscudiero - remove debug stuff
+## 05-19-2017 @ 14.22.32 - (1.0.8)     - dscudiero - Change call name to an absolute 'reports'
