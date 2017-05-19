372250bee2ecf64c9cfb3fc04ba10e87c5adf7d4
David Scudiero <dscudiero@leepfrog.com>
2017-05-19 14:26:50 -0500

    Change call to use absolute name

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,9 +1,10 @@
 ##  #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.2 # -- dscudiero -- Fri 05/19/2017 @ 14:21:53.31
+version=1.0.3 # -- dscudiero -- Fri 05/19/2017 @ 14:26:23.62
 #==================================================================================================
 # Quick call to scriptsAndReports
 #==================================================================================================
 Call scriptsAndReports 'scripts' $*
 ## 05-19-2017 @ 14.22.51 - (1.0.2)     - dscudiero - Change the call name to an absolute 'scripts'
+## 05-19-2017 @ 14.26.50 - (1.0.3)     - dscudiero - Change call to use absolute name
