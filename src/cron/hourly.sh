be227595dc85558f49809ad87f53d580bce85a8b
David Scudiero <dscudiero@leepfrog.com>
2017-05-19 07:26:44 -0500

    add call to reports as a test

--- src/cron/hourly.sh
+++ src/cron/hourly.sh
@@ -1,7 +1,7 @@
 #=======================================================================================================================
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version=2.1.115 # -- dscudiero -- 03/07/2017 @  7:32:27.40
+version=2.1.116 # -- dscudiero -- Fri 05/19/2017 @  7:25:26.05
 #=======================================================================================================================
 # Run every hour from cron
 #=======================================================================================================================
@@ -188,6 +188,10 @@ function BuildToolsAuthTable() {
 #=======================================================================================================================
 case "$hostName" in
 	mojave)
+
+qaEmails='dscudiero@leepfrog.com'
+Call 'scriptsAndReports' 'reports' 'qaStatusShort' "-quiet -email \"$qaEmails\" $scriptArgs"
+
 		## Run perftest on even numberd hours
 		if [[ $(( $(date +"%-H") % 2 )) -eq 0 ]]; then
 			## Make sure we have a sites table before running perfTest
@@ -242,3 +246,4 @@ return 0
 ## Wed Feb 22 15:22:49 CST 2017 - dscudiero - Only run perftest on even numered hours
 ## Fri Feb 24 09:39:09 CST 2017 - dscudiero - Fix a problem checking if the hour was an even hour
 ## Tue Mar  7 07:33:11 CST 2017 - dscudiero - Ignore messages from rsync for SyncInternalDb
+## 05-19-2017 @ 07.26.44 - (2.1.116)   - dscudiero - add call to reports as a test
