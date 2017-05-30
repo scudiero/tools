a2ff9ebfd9a78f03d61b387ac7f53104fe01f2e1
David Scudiero <dscudiero@leepfrog.com>
2017-05-30 07:14:47 -0500

    add processid to the tmp file name

--- lib/MkTmpFile.sh
+++ lib/MkTmpFile.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.8" # -- dscudiero -- 02/07/2017 @ 17:03:37.80
+# version="2.0.9" # -- dscudiero -- Tue 05/30/2017 @  7:14:23.26
 #===================================================================================================
 # Get a temp file name
 #===================================================================================================
@@ -12,9 +12,9 @@ function MkTmpFile {
 	[[ -z $tmpRoot ]] && tmpRoot=/tmp/$LOGNAME
 	[[ ! -d $tmpRoot ]] && mkdir -p $tmpRoot
 	if [[ -z "$1" ]]; then
-		echo "$(mktemp $tmpRoot/$myName.XXXXXXXXXX)"
+		echo "$(mktemp $tmpRoot/$myName.$BASHPID.XXXXXXXXXX)"
 	else
-		echo "$(mktemp $tmpRoot/$myName.$1.XXXXXXXXXX)"
+		echo "$(mktemp $tmpRoot/$myName.$BASHPID.$1.XXXXXXXXXX)"
 	fi
 	return 0
 }
@@ -28,3 +28,4 @@ export -f mkTmpFile
 ## Wed Jan  4 13:54:00 CST 2017 - dscudiero - General syncing of dev to prod
 ## Tue Jan 17 08:57:48 CST 2017 - dscudiero - Make sure there is a value for tmpRoot id defaults were not loaded
 ## Wed Feb  8 10:57:38 CST 2017 - dscudiero - General syncing of dev to prod
+## 05-30-2017 @ 07.14.47 - ("2.0.9")   - dscudiero - add processid to the tmp file name
