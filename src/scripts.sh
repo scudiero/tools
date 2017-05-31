b9b3055f150893a5994adf8bb9734af2b7c33f21
David Scudiero <dscudiero@leepfrog.com>
2017-05-31 07:57:37 -0500

    quote scriptAndReports

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,10 +1,1 @@
-##  #!/bin/bash
-#XO NOT AUTOVERSION
-#==================================================================================================
-version=1.0.3 # -- dscudiero -- Fri 05/19/2017 @ 14:26:23.62
-#==================================================================================================
-# Quick call to scriptsAndReports
-#==================================================================================================
-Call scriptsAndReports 'scripts' $*
-## 05-19-2017 @ 14.22.51 - (1.0.2)     - dscudiero - Change the call name to an absolute 'scripts'
-## 05-19-2017 @ 14.26.50 - (1.0.3)     - dscudiero - Change call to use absolute name
+## 05-31-2017 @ 07.57.37 - dscudiero - quote scriptAndReports
