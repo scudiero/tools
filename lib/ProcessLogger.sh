69032f1484a201ecee838dfebba5baad4042b715
David Scudiero <dscudiero@leepfrog.com>
2017-05-31 15:47:28 -0500

    General syncing of dev to prod

--- lib/ProcessLogger.sh
+++ lib/ProcessLogger.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="1.0.19" # -- dscudiero -- Wed 05/31/2017 @ 15:38:06.68
+# version="1.0.20" # -- dscudiero -- Wed 05/31/2017 @ 15:47:23.10
 #===================================================================================================
 # Write out / update a start record into the process log database
 # Called as 'mode' 'token' 'data'
@@ -23,7 +23,7 @@ function ProcessLogger {
 			[[ $informationOnlyMode == true ]] && argString="${argString}, informationOnlyMode"
 			[[ -n $allItems && $(Contains "$argString" 'allItems') != true ]] && argString="${argString}, AllItems"
 			epochTime=$(date +%s)
-			sqlStmt="insert into $processLogTable (idx,name,hostName,userName,viaScripts,startTime,startEtime,endTime,endEtime,elapsedTime,exitCode,argString,data) \
+			sqlStmt="insert into $processLogTable (idx,name,version,hostName,userName,viaScripts,startTime,startEtime,endTime,endEtime,elapsedTime,exitCode,argString,data) \
 					values(NULL,\"$myName\",\"$version\",\"$hostName\",\"$userName\",\"$calledViaScripts\",\"$startTime\",$(date +%s),NULL,NULL,NULL,NULL,\"$argString\",NULL)"
 			RunSql2 $sqlStmt
 			## Get newly inserted record Id
@@ -79,3 +79,4 @@ export -f ProcessLogger
 ## Wed Jan 11 07:51:24 CST 2017 - dscudiero - Streamlined
 ## 04-13-2017 @ 08.13.02 - ("1.0.18")  - dscudiero - add usage information
 ## 05-31-2017 @ 15.38.24 - ("1.0.19")  - dscudiero - Add the script version to the log record
+## 05-31-2017 @ 15.47.28 - ("1.0.20")  - dscudiero - General syncing of dev to prod
