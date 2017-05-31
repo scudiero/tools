69e5e51ba24eea6e0ba89d4f6e1b5716f6e16aea
David Scudiero <dscudiero@leepfrog.com>
2017-05-31 07:58:02 -0500

    General syncing of dev to prod

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,1 +1,11 @@
-## 05-31-2017 @ 07.57.37 - dscudiero - quote scriptAndReports
+##  #!/bin/bash
+#XO NOT AUTOVERSION
+#==================================================================================================
+version=1.0.4 # -- dscudiero -- Wed 05/31/2017 @  7:57:55.45
+#==================================================================================================
+# Quick call to scriptsAndReports
+#==================================================================================================
+Call 'scriptsAndReports' 'scripts' $*
+## 05-19-2017 @ 14.22.51 - (1.0.2)     - dscudiero - Change the call name to an absolute 'scripts'
+## 05-19-2017 @ 14.26.50 - (1.0.3)     - dscudiero - Change call to use absolute name
+## 05-31-2017 @ 07.58.02 - (1.0.4)     - dscudiero - General syncing of dev to prod
