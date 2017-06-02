0fb62db406b5d712401913843af60781ae8f279d
David Scudiero <dscudiero@leepfrog.com>
2017-06-02 14:24:03 -0500

    General syncing of dev to prod

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "\$* = '$*'"
+echo "\$0 = '$0'"
 if [[ -r $HOME/tools/loader.sh ]]; then
 	"$HOME/tools/loader.sh" $*
 else
@@ -13,3 +14,4 @@ exit
 ## 06-02-2017 @ 14.11.58 - dscudiero - Refectored to call loader.sh
 ## 06-02-2017 @ 14.14.06 - dscudiero - General syncing of dev to prod
 ## 06-02-2017 @ 14.23.14 - dscudiero - add debug
+## 06-02-2017 @ 14.24.03 - dscudiero - General syncing of dev to prod
