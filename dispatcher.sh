1439261de55ccacdef65013d9df717b8d044a735
David Scudiero <dscudiero@leepfrog.com>
2017-06-02 14:49:29 -0500

    Set TOOLSLIBPATH and TOOLSRCPATH

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+export TOOLSLIBPATH="$TOOLSPATH/lib"
+export TOOLSSRCPATH="$TOOLSPATH/src"
+
 loaderDir="$(dirname $0)"
-[[ -r $HOME/tools/loader.sh ]] && loaderDir="$HOME/tools/" || loaderDir="$(dirname $0)"
+if [[ -r $HOME/tools/loader.sh ]]; then
+	loaderDir="$HOME/tools/"
+	[[ -d "$loaderDir/lib" ]] && export TOOLSLIBPATH="$loaderDir/lib:$TOOLSLIBPATH"
+	[[ -d "$loaderDir/src" ]] && export TOOLSSRCPATH="$loaderDir/src:$TOOLSSRCPATH"
+fi
+
 "$loaderDir/loader.sh" $(basename $0) $*
 exit
 
 #===================================================================================================
 ## Check-in log
 #===================================================================================================
-## 06-02-2017 @ 14.11.58 - dscudiero - Refectored to call loader.sh
-## 06-02-2017 @ 14.14.06 - dscudiero - General syncing of dev to prod
-## 06-02-2017 @ 14.23.14 - dscudiero - add debug
-## 06-02-2017 @ 14.24.03 - dscudiero - General syncing of dev to prod
-## 06-02-2017 @ 14.26.56 - dscudiero - General syncing of dev to prod
+
+## 06-02-2017 @ 14.49.29 - dscudiero - Set TOOLSLIBPATH and TOOLSRCPATH
