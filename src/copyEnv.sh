34b5db0cf35f9df6a1f9dfe94cff228dfd726633
David Scudiero <dscudiero@leepfrog.com>
2017-07-18 07:17:23 -0500

    Fix problem in setting nexturl if multiple records exists in default.tcf file

--- src/copyEnv.sh
+++ src/copyEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #DX NOT AUTOVERSION
 #==================================================================================================
-version=4.11.68 # -- dscudiero -- Mon 07/17/2017 @ 14:47:27.83
+version=4.11.71 # -- dscudiero -- Tue 07/18/2017 @  7:14:07.25
 #==================================================================================================
 TrapSigs 'on'
 imports='GetDefaultsData ParseArgs ParseArgsStd Hello Init Goodbye' #
@@ -498,15 +498,12 @@ if [[ $tgtEnv == 'pvt' || $tgtEnv == 'dev' ]]; then
 	# Set nexturl so wf emails point to local instance
 		Msg2 "Changing 'nexturl' to point to local instance..."
 		editFile="$tgtDir/web/courseleaf/localsteps/default.tcf"
-dump editFile
 		clientToken=$(cut -d'/' -f5 <<< $tgtDir)
-dump clientToken
 		toStr="nexturl:https://$clientToken/$(cut -d '/' -f3 <<< $tgtDir).leepfrog.com"
-dump toStr
-		unset grepStr; grepStr=$(ProtectedCall "grep "$toStr" $editFile")
+		unset grepStr; grepStr=$(ProtectedCall "grep "^$toStr" $editFile")
 		if [[ -z $grepStr ]]; then
-			unset fromStr; fromStr=$(ProtectedCall "grep 'nexturl:' $editFile")
-dump fromStr
+			unset fromStr; fromStr=$(ProtectedCall "grep '^nexturl:' $editFile")
+			fromStr=$(CleanString "$fromStr")
 			$DOIT sed -i s"!${fromStr}!${toStr}!" $editFile
 		fi
 
@@ -659,3 +656,4 @@ Goodbye 0 'alert' "$msgText clone from $(ColorK "$(Upper $env)")"
 ## 06-09-2017 @ 16.17.40 - (4.11.65)   - dscudiero - Fix problem of skipping cims and courseleaf if skipCat is active
 ## 06-12-2017 @ 06.57.07 - (4.11.66)   - dscudiero - Fix skipcat
 ## 07-17-2017 @ 14.48.17 - (4.11.68)   - dscudiero - Fix problem with goodbye cleanup removing all of tmpRoot
+## 07-18-2017 @ 07.17.23 - (4.11.71)   - dscudiero - Fix problem in setting nexturl if multiple records exists in default.tcf file
