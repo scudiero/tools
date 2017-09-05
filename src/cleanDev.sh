338194a028c3af69d13bba9bc7ccf5db5bfcc8bc
David Scudiero <dscudiero@leepfrog.com>
2017-09-05 07:09:49 -0500

    more debug stuff

--- src/cleanDev.sh
+++ src/cleanDev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # XO NOT AUTOVERSION
 #==================================================================================================
-version=3.5.8 # -- dscudiero -- Fri 09/01/2017 @ 10:09:48.42
+version=3.5.9 # -- dscudiero -- Tue 09/05/2017 @  7:09:38.62
 #==================================================================================================
 TrapSigs 'on'
 Import ParseArgs ParseArgsStd Hello Init Goodbye
@@ -230,10 +230,11 @@ if [[ -n $client ]]; then
 		fileList="$(ls -d /mnt/dev*/web/*-$searchStr*--AutoDelete*/ 2> /dev/null || true)"
 		for file in $fileList; do
 			file=$(tr -d ':' <<< "$file")
+[[ $userName == 'dscudiero' ]] && echo "file = '$file'"
 			if [[ $(Contains "$file" 'WithSave') == true ]]; then
 				Msg2 "^Deleting '$(basename $file)' with workflow save"
 				quiet=true
-[[ $userName == 'dscudiero ' ]] && echo "Call saveWorkflow -daemon -siteFile \"$file\" -all -suffix \"beforeDelete-$backupSuffix -quiet -nop\""
+[[ $userName == 'dscudiero' ]] && echo "Call saveWorkflow -daemon -siteFile \"$file\" -all -suffix \"beforeDelete-$backupSuffix -quiet -nop\""
 				Call saveWorkflow -daemon -siteFile "$file" -all -suffix "beforeDelete-$backupSuffix -quiet -nop"
 				quiet=false
 				Msg2 "^^workflow saved"
@@ -300,3 +301,4 @@ Goodbye 0
 ## 08-29-2017 @ 08.23.43 - (3.5.6)     - dscudiero - Add some debug messages
 ## 09-01-2017 @ 09.34.21 - (3.5.7)     - dscudiero - Change the way we find the delete sites for daemon calls
 ## 09-01-2017 @ 10.10.32 - (3.5.8)     - dscudiero - Fix syntax problem
+## 09-05-2017 @ 07.09.49 - (3.5.9)     - dscudiero - more debug stuff
