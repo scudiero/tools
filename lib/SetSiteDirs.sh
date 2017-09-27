833a055eec53f2ccab02e68fa70fc8daff61e1a6
David Scudiero <dscudiero@leepfrog.com>
2017-09-27 07:51:27 -0500

    refactored

--- lib/SetSiteDirs.sh
+++ lib/SetSiteDirs.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.18" # -- dscudiero -- 01/12/2017 @ 12:48:01.32
+# version="2.0.20" # -- dscudiero -- Wed 09/27/2017 @  7:43:54.97
 #===================================================================================================
 # Set Directories based on the current hostName name and school name
 # Sets globals: devDir, nextDir, previewDir, publicDir, upgradeDir
@@ -8,75 +8,53 @@
 # Copyright 2016 David Scudiero -- all rights reserved.
 # All rights reserved
 #===================================================================================================
-
 function SetSiteDirs {
-	local mode=$1
-	if [[ $mode == 'check' ]]; then local checkEnv=$2; fi
+	myIncludes="RunSql2"
+	Import "$standardInteractiveIncludes $myIncludes"
 
+	local mode="${1:-setDefault}"; shift || true
+	[[ $mode == 'check' ]] && local checkEnv="$2"
 	[[ -z $client ]] && Terminate "SetSiteDirs: No value for client"
+	local server env checkDir
 
-	local dir=""
 	## Find dev directories
-	for server in $(echo $devServers | tr ',' ' '); do
-		foundClient=false
-		for chkenv in $(echo $courseleafDevEnvs | tr ',' ' '); do
+	unset pvtDir devDir
+	for server in $(tr ',' ' ' <<< "$devServers"); do
+		for env in $(tr ',' ' ' <<< "$courseleafDevEnvs"); do
 			unset ${chkenv}Dir
-			[[ $chkenv == pvt && -d /mnt/$server/web/$client-$userName ]] && eval ${chkenv}Dir=/mnt/$server/web/$client-$userName && foundClient=true && continue
-			[[ $chkenv == dev && -d /mnt/$server/web/$client ]] && eval ${chkenv}Dir=/mnt/$server/web/$client && foundClient=true && continue
+			[[ $env == pvt && -d /mnt/$server/web/$client-$userName ]] && eval pvtDir="/mnt/$server/web/$client-$userName"
+			[[ $env == dev && -d /mnt/$server/web/$client ]] && eval devDir="/mnt/$server/web/$client"
 		done
-		[[ -d "/mnt/$server/web/$client-cim" ]] && testDir="cimDevDir=/mnt/$server/web/$client-cim"
-		[[ $foundClient == true ]] && break
+		[[ -n $pvtDir && -n $devDir ]] && break
 	done
+	[[ -n $devDir ]] && devSiteDir="$devDir" || unset devSiteDir
+	#dump server pvtDir devDir devSiteDir
 
 	## Find production directories
-	skelDir=$skeletonRoot/release
-	for server in $(echo $prodServers | tr ',' ' '); do
-		foundClient=false
-		for chkenv in $(echo $courseleafProdEnvs | tr ',' ' '); do
-			unset ${chkenv}Dir
-			if [[ $chkenv == 'test' ]]; then
-				[[ -d "/mnt/$server/$client-test/test" ]] && testDir="/mnt/$server/$client-test/test"
+	unset testDir currDir previewDir publicDir priorDir
+	for server in $(tr ',' ' ' <<< "$prodServers"); do
+		for env in $(tr ',' ' ' <<< "$courseleafProdEnvs"); do
+			if [[ $env == 'test' ]]; then
+				[[ -d /mnt/$server/$client-$env/$env && -z $testDir ]] && testDir="/mnt/$server/$client-$env/$env"
 			else
-				[[ -d "/mnt/$server/$client/$chkenv" ]] && eval ${chkenv}Dir=/mnt/$server/$client/$chkenv && foundClient=true
+				local token="${env}Dir"
+				[[ -d /mnt/$server/$client/$env && -z ${!token} ]] && eval $token="/mnt/$server/$client/$env"
 			fi
 		done
-		[[ $foundClient == true ]] && break
+		[[ -n $testDir && -n $currDir && -n $previewDir && -n $publicDir && -n $priorDir ]] && break
 	done
+	[[ -n $nextDir ]] && prodSiteDir=$(dirname $nextDir) || unset prodSiteDir
+	#dump server testDir currDir previewDir publicDir priorDir prodSiteDir
 
-	if [[ $mode = 'setDefault' ]]; then
-		if [[ -z $nextDir ]]; then
-			if [[ $noCheck != true ]]; then
-				## Get the share and
-				sqlStmt="select share from $siteInfoTable where name=\"$client\" and env=\"next\""
-				RunSql2 $sqlStmt
-				if [[ ${#resultSet[@]} -gt 0 ]]; then
-					nextDir="/mnt/${resultSet[0]}/$client/next"
-				else
-					Msg2 $T "SetSiteDirs: Mode is $mode and could not resolve the NEXT site directory"
-				fi
-			else
-				nextDir="/mnt/$server/$client/next/"
-			fi
-		fi
-		[[ -z $testDir ]] && testDir=$(sed "s!/next!-test/test!" <<< $nextDir)
-		[[ -z $currDir ]] && currDir=$(sed "s/next/curr/" <<< $nextDir)
-		[[ -z $previewDir ]] && previewDir=$(sed "s/next/preview/" <<< $nextDir)
-		[[ -z $priorDir ]] && priorDir=$(sed "s/next/prior/" <<< $nextDir)
-		[[ -z $publicDir ]] &&  publicDir=$(sed "s/next/public/" <<< $nextDir)
-		[[ -z $devDir ]] && devDir="/mnt/$defaultDevServer/web/$client"
-		[[ -z $pvtDir ]] && pvtDir=$(sed "s!$client!$client-$userName!" <<< $devDir)
-		devSiteDir=$devDir
-		prodSiteDir=$(dirname $nextDir)
-	fi
-
+	## If check mode, make sure the indicated environment diretory exists
 	if [[ $mode == 'check' ]]; then
-		local checkDir='Dir'
-		eval checkDir=\$$checkEnv$checkDir
-		[[ -d $checkDir ]] && echo true || echo false
+		local checkDir="${checkDir}Dir"
+		[[ -d ${!checkDir} ]] && echo true || echo false
 	fi
 
 	return 0
 } #SetSiteDirs
+
 export -f SetSiteDirs
 
 #===================================================================================================
@@ -86,3 +64,4 @@ export -f SetSiteDirs
 ## Wed Jan  4 13:54:27 CST 2017 - dscudiero - General syncing of dev to prod
 ## Thu Jan  5 14:53:53 CST 2017 - dscudiero - refactored checking for test site
 ## Fri Jan  6 14:39:27 CST 2017 - dscudiero - General cleanup , swithch to use -z and -n
+## 09-27-2017 @ 07.51.27 - ("2.0.20")  - dscudiero - refactored
