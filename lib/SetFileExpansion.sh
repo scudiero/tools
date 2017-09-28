70cc31347849a314a4cc778ddb7b0f1a4f38f95e
David Scudiero <dscudiero@leepfrog.com>
2017-09-28 16:03:22 -0500

    Performance tweaks

--- lib/SetFileExpansion.sh
+++ lib/SetFileExpansion.sh
@@ -1,33 +1,28 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.7" # -- dscudiero -- Thu 05/18/2017 @  9:22:47.00
+# version="2.0.14" # -- dscudiero -- Thu 09/28/2017 @ 15:19:50.74
 #===================================================================================================
 # Set the noglob value
 #===================================================================================================
 # Copyright 2016 David Scudiero -- all rights reserved.
 # All rights reserved
 #===================================================================================================
-
 function SetFileExpansion {
 	local mode=$1
-	local prev
 
-	if [[ $mode == '' ]]; then
+	if [[ -z $mode ]]; then
 		if [[ ${#previousFileExpansionSettings[@]} -eq 0 ]]; then
-			previousFileExpansionSettings+=($(set -o | grep noglob | tr "\t" ' ' | tr -s ' ' | cut -d' ' -f2))
-			return 0
+			[[ -z ${-//[^f]/} ]] && previousFileExpansionSettings+=('on') || previousFileExpansionSettings+=('off')
+		else
+			local prev=${previousFileExpansionSettings[${#previousFileExpansionSettings[@]}-1]}
+			unset previousFileExpansionSettings[${#previousFileExpansionSettings[@]}-1]
+			[[ $prev == 'on' ]] && set -o noglob || set +o noglob
 		fi
-
-		## Toggle value
-		prev=${previousFileExpansionSettings[${#previousFileExpansionSettings[@]}-1]}
-		unset previousFileExpansionSettings[${#previousFileExpansionSettings[@]}-1]
-		[[ $prev == 'on' ]] && set -o noglob || set +o noglob
-		return 0
+	else
+		[[ -z ${-//[^f]/} ]] && previousFileExpansionSettings+=('on') || previousFileExpansionSettings+=('off')
+		[[ $mode == 'on' || $mode == 'ON' || $mode == 'On' ]] && set +o noglob || set -o noglob
 	fi
-
-	previousFileExpansionSettings+=($(set -o | grep noglob | tr "\t" ' ' | tr -s ' ' | cut -d' ' -f2))
-	[[ $(Lower $mode) == 'on' ]] && set +o noglob || set -o noglob
-
+	#echo ${previousFileExpansionSettings[*]}
 	return 0
 } #SetFileExpansion
 export -f SetFileExpansion
@@ -38,3 +33,4 @@ export -f SetFileExpansion
 
 ## Wed Jan  4 13:54:25 CST 2017 - dscudiero - General syncing of dev to prod
 ## 05-18-2017 @ 09.31.34 - ("2.0.7")   - dscudiero - Switch to use set -o for clarification
+## 09-28-2017 @ 16.03.22 - ("2.0.14")  - dscudiero - Performance tweaks
