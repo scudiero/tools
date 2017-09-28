2e1e7c83bdf3df3c1601ed81aa5e8d00469e16a3
David Scudiero <dscudiero@leepfrog.com>
2017-09-28 13:45:09 -0500

    Performance tweaks

--- lib/Import.sh
+++ lib/Import.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="1.0.59" # -- dscudiero -- Thu 09/07/2017 @ 11:16:22.43
+# version="1.1.0" # -- dscudiero -- Thu 09/28/2017 @ 13:30:10.83
 #===================================================================================================
 # Import need functions into the runtime environment
 #===================================================================================================
@@ -8,48 +8,28 @@
 # All rights reserved
 #===================================================================================================
 function Import {
-	function MyContains {
-		[[ $# -lt 2 ]] && echo false && return 0
-		local string="$1"
-		local substring="$2"
-		local testStr=${string#*$substring}
-
-		[[ "$testStr" != "$string" ]] && echo true || echo false
-		return 0
-	} #MyContains
-
-	includeList="$*"
+	local includeList="$*"
 	[[ -z $includeList ]] && return 0
-	local searchDirs includeName found token
+	local searchDirs includeName found
 
-	[[ $TOOLSLIBPATH == '' ]] && searchDirs="$TOOLSPATH/lib" || searchDirs="$( tr ':' ' ' <<< $TOOLSLIBPATH)"
+	[[ -z $TOOLSLIBPATH ]] && searchDirs="$TOOLSPATH/lib" || searchDirs="$( tr ':' ' ' <<< $TOOLSLIBPATH)"
 
-	#echo -e"\nincludeList = '$includeList'"
-	#echo -e"\tSCRIPTINCLUDES Initial = '$SCRIPTINCLUDES'"
 	## Search for the include file, load the first one
 	for includeName in $includeList; do
-		#echo -e "\tincludeName = '$includeName'\t\t$SCRIPTINCLUDES"
-		[[ $(MyContains ",$SCRIPTINCLUDES," ",$includeName,") == true ]] && continue
+		#[[ $(MyContains ",$SCRIPTINCLUDES," ",$includeName,") == true ]] && continue
+		[[ "${SCRIPTINCLUDES#*$includeName}" != "$SCRIPTINCLUDES" ]] && continue  ## i.e. SCRIPTINCLUDES contains includeName
 		found=false
 		for searchDir in $searchDirs; do
 			if [[ -r ${searchDir}/${includeName}.sh ]]; then
-				unset includes imports
 				source ${searchDir}/${includeName}.sh
-				[[ -z $SCRIPTINCLUDES ]] && SCRIPTINCLUDES="$includeName" || SCRIPTINCLUDES="$SCRIPTINCLUDES,$includeName"
-				#echo -e "\t\tSCRIPTINCLUDES = '$SCRIPTINCLUDES'"
-				#echo -e "\t\${includes}\${imports}  = '${includes}${imports} '"
-				if [[ -n ${includes}${imports} ]]; then
-					for token in ${includes} ${imports}; do
-						[[ $(MyContains ",$SCRIPTINCLUDES," ",$token,") != true ]] && SCRIPTINCLUDES="$SCRIPTINCLUDES,$token"
-					done
-				fi
+				SCRIPTINCLUDES="$SCRIPTINCLUDES,$includeName"
 				found=true
+				break
 			fi
-			[[ $found == true ]] && break ## searchDir
 		done
-		[[ $found != true ]] && echo -e "\n*** Error, Import function, cannot locate '$includeName' *** \n" && exit
+		[[ $found != true ]] && echo -e "\n*** Error, Import function, cannot locate '$includeName' *** \n"
 	done
-	#echo "SCRIPTINCLUDES Final = '$SCRIPTINCLUDES'";echo;echo
+	[[ ${SCRIPTINCLUDES:0:1} == ',' ]] && SCRIPTINCLUDES="${SCRIPTINCLUDES:2}"
 	return 0
 } #Import
 export -f Import
@@ -58,3 +38,4 @@ export -f Import
 # Check-in Log
 #===================================================================================================
 ## Wed Jan  4 13:53:44 CST 2017 - dscudiero - General syncing of dev to prod
+## 09-28-2017 @ 13.45.08 - ("1.1.0")   - dscudiero - Performance tweaks
