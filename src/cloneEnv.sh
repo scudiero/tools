2464ce49f420b1b4d4182f4ea5a253aa6e3a981e
David Scudiero <dscudiero@leepfrog.com>
2017-09-29 15:34:10 -0500

    Switch to use FineExecutable vs Call

--- src/cloneEnv.sh
+++ src/cloneEnv.sh
@@ -1,16 +1,14 @@
 ##  #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.4 # -- dscudiero -- Fri 09/29/2017 @ 15:31:45.93
+version=1.0.5 # -- dscudiero -- Fri 09/29/2017 @ 15:33:49.38
 #==================================================================================================
 # Quick call to scriptsAndReports
 #==================================================================================================
-[[ $userName == 'dscudiero' ]] && Here CloneEnv 0
 Import 'FindExecutable'
 executeFile=$(FindExecutable 'copyEnv')
-[[ $userName == 'dscudiero' ]] && echo && echo "HERE 2" && echo "executeFile = '$executeFile'"
-
 source $executeFile -tgtEnv pvt $scriptArgs
 ## 09-29-2017 @ 15.26.27 - (1.0.2)     - dscudiero - General syncing of dev to prod
 ## 09-29-2017 @ 15.30.08 - (1.0.3)     - dscudiero - Add debug stuff
 ## 09-29-2017 @ 15.31.55 - (1.0.4)     - dscudiero - General syncing of dev to prod
+## 09-29-2017 @ 15.34.10 - (1.0.5)     - dscudiero - Switch to use FineExecutable vs Call
