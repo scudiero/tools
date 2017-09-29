35ec077682bd9b9fbfaec9625ed8559df260c770
David Scudiero <dscudiero@leepfrog.com>
2017-09-29 15:30:02 -0500

    Add debug stuff

--- loader.sh
+++ loader.sh
@@ -251,7 +251,7 @@ sTime=$(date "+%s")
 		TrapSigs 'off'
 		trap "CleanUp" EXIT ## Set trap to return here for cleanup
 		[[ $(cut -d' ' -f1 <<< $(wc -l "$executeFile")) -eq 0 ]] && Terminate "Execution file ($executeFile) is empty"
-[[ $userName == 'dscudiero' ]] && echo && echo "HERE" && echo "executeFile = '$executeFile'"
+[[ $userName == 'dscudiero' ]] && echo && echo "HERE 1" && echo "executeFile = '$executeFile'"
 		source $executeFile $scriptArgs 2>&1 | tee -a $logFile; rc=$?
 
 ## Should never get here but just in case
@@ -378,3 +378,4 @@ sTime=$(date "+%s")
 ## 09-29-2017 @ 13.25.00 - ("1.3.95")  - dscudiero - General syncing of dev to prod
 ## 09-29-2017 @ 15.14.55 - ("1.4.-1")  - dscudiero - add debug
 ## 09-29-2017 @ 15.16.39 - ("1.4.-1")  - dscudiero - General syncing of dev to prod
+## 09-29-2017 @ 15.30.02 - ("1.4.-1")  - dscudiero - Add debug stuff
