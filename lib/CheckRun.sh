14c0d45428b78c2500520e7eb40ae6a0d1736979
David Scudiero <dscudiero@leepfrog.com>
2017-09-29 13:29:43 -0500

    Include RunSql2

--- lib/CheckRun.sh
+++ lib/CheckRun.sh
@@ -2,7 +2,7 @@
 
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.11" # -- dscudiero -- Wed 09/06/2017 @ 14:14:20.61
+# version="2.0.12" # -- dscudiero -- Fri 09/29/2017 @ 13:29:11.44
 #===================================================================================================
 ## Check to see if the current excution environment supports script execution
 ## Returns 1 in $? if user is authorized, otherwise it returns 0
@@ -13,6 +13,7 @@
 #===================================================================================================
 
 function CheckRun {
+	Include 'RunSql2'
 	local script=${1:-$myName}
 	local tempStr grepOut os host sqlStmt resultString
 
@@ -42,3 +43,4 @@ export -f CheckRun
 ## 05-05-2017 @ 13.45.20 - ("2.0.8")   - dscudiero - General syncing of dev to prod
 ## 05-09-2017 @ 13.57.17 - ("2.0.10")  - dscudiero - Refactored to improve performance
 ## 09-06-2017 @ 14.14.46 - ("2.0.11")  - dscudiero - Updateed to check if active is not Yes and not N/A
+## 09-29-2017 @ 13.29.43 - ("2.0.12")  - dscudiero - Include RunSql2
