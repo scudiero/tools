a9be59483249072168bc248c1ac997a9837c4aa0
David Scudiero <dscudiero@leepfrog.com>
2017-10-02 13:14:25 -0500

    General syncing of dev to prod

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,13 +1,17 @@
 ##  #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.5 # -- dscudiero -- Wed 09/13/2017 @  8:31:23.83
+version=1.0.6 # -- dscudiero -- Mon 10/02/2017 @ 13:14:14.51
 #==================================================================================================
 # Quick call to scriptsAndReports
 #==================================================================================================
-Import "Call"
-Call 'scriptsAndReports' 'scripts' $*
+Import 'scriptsAndReports'
+executeFile=$(FindExecutable 'copyEnv')
+myName="$(cut -d'.' -f1 <<< $(basename $executeFile))"
+myPath="$(dirname $executeFile)"
+source $executeFile 'scripts' $*
 ## 05-19-2017 @ 14.22.51 - (1.0.2)     - dscudiero - Change the call name to an absolute 'scripts'
 ## 05-19-2017 @ 14.26.50 - (1.0.3)     - dscudiero - Change call to use absolute name
 ## 05-31-2017 @ 07.58.02 - (1.0.4)     - dscudiero - General syncing of dev to prod
 ## 09-13-2017 @ 08.33.06 - (1.0.5)     - dscudiero - Import Call before usage
+## 10-02-2017 @ 13.14.25 - (1.0.6)     - dscudiero - General syncing of dev to prod
