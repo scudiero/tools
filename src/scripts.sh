e6e494e182e78f78f24965c35499d1322a08ebe6
David Scudiero <dscudiero@leepfrog.com>
2017-10-02 13:15:33 -0500

    General syncing of dev to prod

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,12 +1,12 @@
 ##  #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.6 # -- dscudiero -- Mon 10/02/2017 @ 13:14:14.51
+version=1.0.7 # -- dscudiero -- Mon 10/02/2017 @ 13:15:22.69
 #==================================================================================================
 # Quick call to scriptsAndReports
 #==================================================================================================
-Import 'scriptsAndReports'
-executeFile=$(FindExecutable 'copyEnv')
+Import 'FindExecutable'
+executeFile=$(FindExecutable 'scriptsAndReports')
 myName="$(cut -d'.' -f1 <<< $(basename $executeFile))"
 myPath="$(dirname $executeFile)"
 source $executeFile 'scripts' $*
@@ -15,3 +15,4 @@ source $executeFile 'scripts' $*
 ## 05-31-2017 @ 07.58.02 - (1.0.4)     - dscudiero - General syncing of dev to prod
 ## 09-13-2017 @ 08.33.06 - (1.0.5)     - dscudiero - Import Call before usage
 ## 10-02-2017 @ 13.14.25 - (1.0.6)     - dscudiero - General syncing of dev to prod
+## 10-02-2017 @ 13.15.32 - (1.0.7)     - dscudiero - General syncing of dev to prod
