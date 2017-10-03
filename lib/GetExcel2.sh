2308e723a93e1bc1b82840a5cc3833950890756a
David Scudiero <dscudiero@leepfrog.com>
2017-10-03 07:06:23 -0500

    REformat comments

--- lib/GetExcel2.sh
+++ lib/GetExcel2.sh
@@ -14,20 +14,24 @@ function GetExcel2 {
 	myIncludes="SetFileExpansion StringFunctions FindExecutable ProtectedCall InitializeInterpreterRuntime Msg3 Goodbye"
 	Import "$standardIncludes $myIncludes"
 
-	local workBook workSheet delimiter='|'
-	local tmpFile=$(MkTmpFile $FUNCNAME)
-	local verboseLevelSave=$verboseLevel;
-	while [[ $# -gt 0 ]]; do
-	    [[ $1 =~ ^-wb|--workbook$ ]] && { workBook="$2"; shift 2; continue; }
-	    [[ $1 =~ ^-ws|--workSheet$ ]] && { workSheet="$2"; shift 2; continue; }
-	    [[ $1 =~ ^-d|--delimiter$ ]] && { delimiter="$2"; shift 2; continue; }
-	    [[ -z $workBook ]] && { workBook="$1"; shift 1 || true; continue; }
-	    workSheet="$workSheet $1"
-	    shift 1 || true
-	done
-	[[ ${workSheet:0:1} == ' ' ]] && workSheet="${workSheet:1}"
-
 	function GetExcelCleanup { SetFileExpansion 'on' ; rm -rf ${tmpFile}* >& /dev/null ; SetFileExpansion ; }
+
+	## Local defaults
+		local workBook workSheet delimiter='|'
+		local tmpFile=$(MkTmpFile $FUNCNAME)
+		local verboseLevelSave=$verboseLevel;
+
+	## Parse defaults
+		while [[ $# -gt 0 ]]; do
+		    [[ $1 =~ ^-wb|--workbook$ ]] && { workBook="$2"; shift 2; continue; }
+		    [[ $1 =~ ^-ws|--workSheet$ ]] && { workSheet="$2"; shift 2; continue; }
+		    [[ $1 =~ ^-d|--delimiter$ ]] && { delimiter="$2"; shift 2; continue; }
+		    [[ -z $workBook ]] && { workBook="$1"; shift 1 || true; continue; }
+		    workSheet="$workSheet $1"
+		    shift 1 || true
+		done
+		[[ ${workSheet:0:1} == ' ' ]] && workSheet="${workSheet:1}"
+
 
 	[[ -z $workBook || ! -f $workBook ]] && Terminate "$FUNCNAME: Could not locate workbook file '$workBook'"
 	if [[ $(Contains "$workBook" ' ') == true ]]; then
@@ -40,30 +44,32 @@ function GetExcel2 {
 	[[ -z $executeFile ]] && Terminate "$myName.sh.$LINENO: Could not resolve the script source file:\n\t$executeFile"
 
 	## Call the 'real' program to parse the spreadsheet
-	PYDIR="$TOOLSPATH/Python/linux/64/3.4.3.2"
-	export PYDIR="$PYDIR"
-	pathSave="$PATH"
-	export PATH="$PYDIR:$PATH"
-	verboseLevel=0
-	cmdStr="$PYDIR/bin/python -u $executeFile "$workBook" "$workSheet" |"
- 	$cmdStr > "$tmpFile"
- 	verboseLevel=$verboseLevelSave
-	export PATH="$pathSave"
+		PYDIR="$TOOLSPATH/Python/linux/64/3.4.3.2"
+		export PYDIR="$PYDIR"
+		pathSave="$PATH"
+		export PATH="$PYDIR:$PATH"
+		verboseLevel=0
+		cmdStr="$PYDIR/bin/python -u $executeFile "$workBook" "$workSheet" |"
+	 	$cmdStr > "$tmpFile"
+	 	verboseLevel=$verboseLevelSave
+		export PATH="$pathSave"
 
-	local grepStr=$(ProtectedCall "grep '*Fatal Error*' $tmpFile")
-	[[ $grepStr == '' ]] && grepStr=$(ProtectedCall "grep '*Error*' $tmpFile")
-	if [[ $grepStr != '' || $(tail -n 1 $tmpFile) == '-1' ]]; then
-		Error "Could not retrieve data from workbook, please see below"
-		tail -n 10 $tmpFile > $tmpFile.2
-		while read -r line; do echo -e "\t$line"; done < $tmpFile.2;
-		GetExcelCleanup
-		Msg3
-		Goodbye 1
-	fi
+	## Check for errors
+		local grepStr=$(ProtectedCall "grep '*Fatal Error*' $tmpFile")
+		[[ $grepStr == '' ]] && grepStr=$(ProtectedCall "grep '*Error*' $tmpFile")
+		if [[ $grepStr != '' || $(tail -n 1 $tmpFile) == '-1' ]]; then
+			Error "Could not retrieve data from workbook, please see below"
+			tail -n 10 $tmpFile > $tmpFile.2
+			while read -r line; do echo -e "\t$line"; done < $tmpFile.2;
+			GetExcelCleanup
+			Msg3
+			Goodbye 1
+		fi
 
 	## Set output to an array
-	unset resultSet
-	IFS=$'\n' read -rd '' -a resultSet < "$tmpFile"
+		unset resultSet
+		IFS=$'\n' read -rd '' -a resultSet < "$tmpFile"
+
 	GetExcelCleanup
 	return 0
 } #GetExcel2
@@ -83,3 +89,4 @@ export -f GetExcel2
 ## 05-18-2017 @ 12.35.13 - ("2.0.67")  - dscudiero - Remove the -vv from the call to getXlsx.py
 ## 09-20-2017 @ 12.09.52 - ("2.0.69")  - dscudiero - Add protectedcall to includes list
 ## 10-02-2017 @ 17.07.34 - ("2.1.0")   - dscudiero - Return a bad condition code if data retrieval fails
+## 10-03-2017 @ 07.06.23 - ("2.1.0")   - dscudiero - REformat comments
