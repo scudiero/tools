35fd8e4d5f596c159f2227b6cdf5d556672bb7f6
David Scudiero <dscudiero@leepfrog.com>
2017-10-03 16:14:01 -0500

    Refactored to allow for report of all online scripts

--- src/online.sh
+++ src/online.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #===================================================================================================
-version=1.0.13 # -- dscudiero -- Tue 10/03/2017 @  7:36:39.52
+version=1.0.23 # -- dscudiero -- Tue 10/03/2017 @ 16:12:52.56
 #===================================================================================================
 TrapSigs 'on'
 myIncludes="RunSql2"
@@ -13,17 +13,29 @@ scriptDescription="Bring a tools script online"
 ## turn a script offline -- i.e. create an .offline file
 #===================================================================================================
 GetDefaultsData $myName
-ParseArgsStd
 
-script=$client
-Prompt script "Please specify the script to take online" '*any*'
+if [[ -n $originalArgStr ]]; then
+	for script in $originalArgStr; do
+		[[ ${script: (-3)} == '.sh' ]] && script="$(cut -d'.' -f1 <<< $script)"
+		sqlStmt="update $scriptsTable set active=\"Yes\" where name=\"$script\""
+		RunSql2 $sqlStmt
+		Msg3 "^$script is now online"
+	done
+else
+	Msg3 "Current online scripts:"
+	sqlStmt="select name,showInScripts from $scriptsTable where active=\"Yes\" order by showInScripts,name"
+	RunSql2 $sqlStmt
+	for result in ${resultSet[@]}; do
+		Msg3 "^${result%%|*}^${result##*|}" 
+	done
+	Msg3
+fi
 
-[[ ${script: (-3)} == '.sh' ]] && script="$(cut -d'.' -f1 <<< $script)"
-sqlStmt="update $scriptsTable set active=\"Yes\" where name=\"$script\""
-RunSql2 $sqlStmt
+Goodbye
 #===================================================================================================
 ## Check-in log
 #===================================================================================================
 ## Thu May  5 09:21:05 CDT 2016 - dscudiero - Switch to set offline in the database
 ## Fri Jul 15 13:22:53 CDT 2016 - dscudiero - General syncing of dev to prod
 ## 10-03-2017 @ 07.39.17 - (1.0.13)    - dscudiero - Add RunSql2 to the include list
+## 10-03-2017 @ 16.14.01 - (1.0.23)    - dscudiero - Refactored to allow for report of all online scripts
