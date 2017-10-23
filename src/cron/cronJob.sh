7793559540f1f356433d5e670fc0592afb462950
David Scudiero <dscudiero@leepfrog.com>
2017-10-23 11:03:55 -0500

    Make sure the permissions of the log files is 644

--- src/cron/cronJob.sh
+++ src/cron/cronJob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version=2.1.12 # -- dscudiero -- Thu 10/19/2017 @ 15:12:13.90
+version=2.1.13 # -- dscudiero -- Mon 10/23/2017 @ 11:00:23.78
 #=======================================================================================================================
 # Cron task initiator
 #=======================================================================================================================
@@ -56,7 +56,7 @@ originalArgStr="$*"
 	echo -e "\t-- $hostName - $callScriptName done" >> $TOOLSPATH/Logs/cronJobs/cronJobs.log
 	mv $logFile $logFile.bak
  	cat $logFile.bak | sed "s/\x1B\[[0-9;]*[a-zA-Z]//g" | tr -d '\007' > $logFile
-	chmod ug+rwx "$logFile"
+	chmod 644 "$logFile"
  	rm $logFile.bak
 	touch "$(dirname $logFile)"
 	myName="$myNameSave"; myPath="$myPathSave"
@@ -123,3 +123,4 @@ exit 0
 ## 10-17-2017 @ 14.07.48 - (2.1.10)    - dscudiero - Make sure myName is set correctly
 ## 10-19-2017 @ 12.19.35 - (2.1.11)    - dscudiero - touch the logFile upon return to set time date stamp
 ## 10-19-2017 @ 15.12.53 - (2.1.12)    - dscudiero - Cleanup the logFile from called task
+## 10-23-2017 @ 11.03.55 - (2.1.13)    - dscudiero - Make sure the permissions of the log files is 644
