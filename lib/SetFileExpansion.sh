afdfdb84e1aa619c5a274ed19726ba79692dabf7
David Scudiero <dscudiero@leepfrog.com>
2017-10-27 14:51:19 -0500

    Cosmetic/minor change

--- lib/SetFileExpansion.sh
+++ lib/SetFileExpansion.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.15" # -- dscudiero -- Fri 10/27/2017 @ 14:42:07.41
+# version="2.0.19" # -- dscudiero -- Fri 10/27/2017 @ 14:51:14.11
 #===================================================================================================
 # Set the noglob value
 #===================================================================================================
@@ -8,7 +8,7 @@
 # All rights reserved
 #===================================================================================================
 function SetFileExpansion {
-	local mode=$1 lastVal
+	local mode=$1
 
 	if [[ $mode == 'on' ]]; then
 		previousFileExpansionSettings+=('on')
@@ -18,12 +18,11 @@ function SetFileExpansion {
 		if [[ ${#previousFileExpansionSettings[@]} -eq 0 ]]; then
 			previousFileExpansionSettings+=("$(set -o | grep noglob | cut -d' ' -f3)")
 		else
-			lastVal=${previousFileExpansionSettings[-1]}
+			[[ ${previousFileExpansionSettings[@]:(-1)} == 'on' ]] && set +o noglob || set -o noglob
 			unset 'previousFileExpansionSettings[${#previousFileExpansionSettings[@]}-1]'
-			[[ $lastVal == 'on' ]] && set +o noglob || set -o noglob
 		fi
 	fi
-echo ${previousFileExpansionSettings[*]}
+	#echo ${previousFileExpansionSettings[*]}
 	return 0
 } #SetFileExpansion
 export -f SetFileExpansion
@@ -31,8 +30,8 @@ export -f SetFileExpansion
 #===================================================================================================
 # Check-in Log
 #===================================================================================================
-
 ## Wed Jan  4 13:54:25 CST 2017 - dscudiero - General syncing of dev to prod
 ## 05-18-2017 @ 09.31.34 - ("2.0.7")   - dscudiero - Switch to use set -o for clarification
 ## 09-28-2017 @ 16.03.22 - ("2.0.14")  - dscudiero - Performance tweaks
 ## 10-27-2017 @ 14.42.40 - ("2.0.15")  - dscudiero - Refactor to make simpler
+## 10-27-2017 @ 14.51.19 - ("2.0.19")  - dscudiero - Cosmetic/minor change
