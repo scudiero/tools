42e0771605042b6c4c43581e3ac21b9437aaad5b
David Scudiero <dscudiero@leepfrog.com>
2017-12-04 08:25:27 -0600

    Updated to add arguments for parameters and switch to Msg3

--- src/clientMoved.sh
+++ src/clientMoved.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 #XO NOT AUTOVERSION
 #==================================================================================================
-version=1.0.38 # -- dscudiero -- Thu 09/14/2017 @ 14:47:46.25
+version=1.0.47 # -- dscudiero -- Mon 12/04/2017 @  8:24:49.44
 #==================================================================================================
 TrapSigs 'on'
-includes='Msg2 Dump GetDefaultsData ParseArgsStd Hello DbLog Init Goodbye VerifyContinue MkTmpFile'
-includes="$includes RunSql2 SelectMenuNew RunCourseLeafCgi"
-Import "$includes"
+myIncludes="SelectMenuNew RunCourseLeafCgi"
+Import "$standardInteractiveIncludes $myIncludes"
+
 originalArgStr="$*"
 scriptDescription=""
+
+#==================================================================================================
+# clients=$(ls /mnt/bluestone | grep -v 'test')
+# for client in $clients; do
+# 	Msg3 "Processing:" $client
+# 	clientMoved --useLocal $client -newh "$newHost" -newprods "$newProdServer" -newdevs "$newDevServer" -nop -quiet
+# done
+#==================================================================================================
 
 #= Description +===================================================================================
 #
@@ -17,17 +25,18 @@ scriptDescription=""
 #==================================================================================================
 # Standard call back functions
 #==================================================================================================
-function parseArgs-clientMoved  { # or parseArgs-local
-	#argList+=(-optionArg,1,option,scriptVar,,script,'Help text')
-	#argList+=(-flagArg,2,switch,scriptVar,,script,'Help text')
-	argList+=(-file,4,option,file,,script,'The file name relative to the root site directory')
+function clientMoved-ParseArgsStd2  { # or parseArgs-local
+		myArgs+=('newh|newhost|option|newHost||script|The new host name for the client')
+		myArgs+=('newdevs|newdevshare|option|newDevShare||script|The new dev server for the client')
+		myArgs+=('newprods|newprodshare|option|newProdShare||script|The new production server for the client')
+		myArgs+=('myf|myflag|switch|myFlag||script|Mp flag variable')
 	return 0
 }
-function Goodbye-clientMoved  { # or Goodbye-local
+function clientMoved-Goodbye  { # or Goodbye-local
 	rm -rf $tmpRoot > /dev/null 2>&1
 	return 0
 }
-function testMode-clientMoved  { # or testMode-local
+function clientMoved-testMode  { # or testMode-local
 	return 0
 }
 
@@ -57,9 +66,9 @@ helpSet='script,client'
 scriptHelpDesc="This script can be used to update the warehouse data when a client is moved to a new server"
 
 GetDefaultsData $myName
-ParseArgsStd
+ParseArgsStd2 $originalArgStr
 Hello
-Init "getClient"
+Init "getClient nocheck"
 
 unset origHost origDevShare origProdShare
 ## Get current information
@@ -76,52 +85,58 @@ unset origHost origDevShare origProdShare
 	fi
 
 ## New host name
-	if [[ $(grep -o ',' <<< "$linuxHosts" | wc -l) -gt 1 ]]; then
-		menuList=("|New Host")
-		for token in $(tr ',' ' ' <<< $linuxHosts); do
-			[[ $origHost != '' && $origHost == $token ]] && continue
-			menuList+=("|${token}")
-		done
-		Msg2; Msg2 "Please specify the ordinal number of the new linux host."
-		Note "If the client's new host is not listed here then the site's data has already been updated."
-		SelectMenuNew 'menuList' 'newHost' '\nHost ordinal (or 'x' to quit) > '
-		[[ $newHost == '' ]] && Goodbye 'quickquit' || newHost=$(cut -d'|' -f1 <<< $newHost)
-	else
-		newHost=$linuxHosts
+	if [[ -z $newHost ]]; then
+		if [[ $(grep -o ',' <<< "$linuxHosts" | wc -l) -gt 1 ]]; then
+			menuList=("|New Host")
+			for token in $(tr ',' ' ' <<< $linuxHosts); do
+				[[ $origHost != '' && $origHost == $token ]] && continue
+				menuList+=("|${token}")
+			done
+			Msg3; Msg3 "Please specify the ordinal number of the new linux host."
+			Note "If the client's new host is not listed here then the site's data has already been updated."
+			SelectMenuNew 'menuList' 'newHost' '\nHost ordinal (or 'x' to quit) > '
+			[[ $newHost == '' ]] && Goodbye 'quickquit' || newHost=$(cut -d'|' -f1 <<< $newHost)
+		else
+			newHost=$linuxHosts
+		fi
 	fi
 
 ## New Production Server
-	sqlStmt="Select value from defaults where name=\"prodServers\" and host=\"$newHost\""
-	RunSql2 $sqlStmt
-	defaultProdShares=${resultSet[0]}
-	if [[ $(grep -o ',' <<< "$defaultProdShares" | wc -l) -gt 1 ]]; then
-		menuList=("|New Prod Server")
-		for token in $(tr ',' ' ' <<< $defaultProdShares); do
-			[[ $origProdShare != '' && $origProdShare == $token ]] && continue
-			menuList+=("|${token}")
-		done
-		Msg2; Msg2 "Please specify the ordinal number of the new production server:"
-		SelectMenuNew 'menuList' 'newProdShare' '\nServerHost ordinal (or 'x' to quit) > '
-		[[ $newProdShare == '' ]] && Goodbye 'quickquit' || newProdShare=$(cut -d'|' -f1 <<< $newProdShare)
-	else
-		newProdShare=$defaultProdShares
+	if [[ -z $newProdShare ]]; then
+		sqlStmt="Select value from defaults where name=\"prodServers\" and host=\"$newHost\""
+		RunSql2 $sqlStmt
+		defaultProdShares=${resultSet[0]}
+		if [[ $(grep -o ',' <<< "$defaultProdShares" | wc -l) -gt 1 ]]; then
+			menuList=("|New Prod Server")
+			for token in $(tr ',' ' ' <<< $defaultProdShares); do
+				#[[ $origProdShare != '' && $origProdShare == $token ]] && continue
+				menuList+=("|${token}")
+			done
+			Msg3; Msg3 "Please specify the ordinal number of the new production server:"
+			SelectMenuNew 'menuList' 'newProdShare' '\nServerHost ordinal (or 'x' to quit) > '
+			[[ $newProdShare == '' ]] && Goodbye 'quickquit' || newProdShare=$(cut -d'|' -f1 <<< $newProdShare)
+		else
+			newProdShare=$defaultProdShares
+		fi
 	fi
 
 ## New Dev Server
-	sqlStmt="Select value from defaults where name=\"devServers\" and host=\"$newHost\""
-	RunSql2 $sqlStmt
-	defaultDevShares=${resultSet[0]}
-	if [[ $(grep -o ',' <<< "$defaultDevShares" | wc -l) -gt 1 ]]; then
-		menuList=("|New Dev Server")
-		for token in $(tr ',' ' ' <<< $defaultDevShares); do
-			[[ $origDevShare != '' && $origDevShare == $token ]] && continue
-			menuList+=("|${token}")
-		done
-		Msg2; Msg2 "Please specify the ordinal number of the new production server:"
-		SelectMenuNew 'menuList' 'newDevShare' '\nServerHost ordinal (or 'x' to quit) > '
-		[[ $newDevShare == '' ]] && Goodbye 'quickquit' || newDevShare=$(cut -d'|' -f1 <<< $newDevShare)
-	else
-		newDevShare=$defaultDevShares
+	if [[ -z $newDevShare ]]; then
+		sqlStmt="Select value from defaults where name=\"devServers\" and host=\"$newHost\""
+		RunSql2 $sqlStmt
+		defaultDevShares=${resultSet[0]}
+		if [[ $(grep -o ',' <<< "$defaultDevShares" | wc -l) -gt 1 ]]; then
+			menuList=("|New Dev Server")
+			for token in $(tr ',' ' ' <<< $defaultDevShares); do
+				[[ $origDevShare != '' && $origDevShare == $token ]] && continue
+				menuList+=("|${token}")
+			done
+			Msg3; Msg3 "Please specify the ordinal number of the new production server:"
+			SelectMenuNew 'menuList' 'newDevShare' '\nServerHost ordinal (or 'x' to quit) > '
+			[[ $newDevShare == '' ]] && Goodbye 'quickquit' || newDevShare=$(cut -d'|' -f1 <<< $newDevShare)
+		else
+			newDevShare=$defaultDevShares
+		fi
 	fi
 
 unset verifyArgs
@@ -138,7 +153,7 @@ myData="Client: '$client', New Host: '$newHost', New Dev Share: '$newDevShare', 
 # Main
 #===================================================================================================
 
-sqlStmt="Update $siteInfoTable set host=\"$newHost\" where name=\"$client\""
+sqlStmt="Update $siteInfoTable set host=\"$newHost\" where name=\"$client\" and host is not null and host <> \"N/A\""
 RunSql2 $sqlStmt
 
 sqlStmt="Update $siteInfoTable set share=\"$newDevShare\" where name=\"$client\" and env=\"dev\""
@@ -147,25 +162,25 @@ RunSql2 $sqlStmt
 sqlStmt="Update $siteInfoTable set share=\"$newProdShare\" where name=\"${client}\" and env not in (\"dev\",\"preview\",\"public\")"
 RunSql2 $sqlStmt
 
-sqlStmt="Update $siteInfoTable set host=\"$newHost\" where name=\"${client}-test\" and env=\"test\""
+sqlStmt="Update $siteInfoTable set host=\"$newHost\" where name=\"${client}-test\" and env=\"test\" and host is not null and host <> \"N/A\""
 RunSql2 $sqlStmt
 sqlStmt="Update $siteInfoTable set share=\"$newProdShare\" where name=\"${client}-test\" and env=\"test\""
 RunSql2 $sqlStmt
 
-Msg2 "Data Warehouse data updated to reflect the clients new location"
+Msg3 "Data Warehouse data updated to reflect the clients new location"
 
 lfinternal=$(ProtectedCall "grep lfinternal /etc/group")
 if [[ $lfinternal != '' ]]; then
 	if [[ $(Contains "$lfinternal" "$userName") == true ]]; then
 		RunCourseLeafCgi "$stageInternal" "-r /clients/$client"
-		Msg2 "Internal client page republished to reflect change"
+		Msg3 "Internal client page republished to reflect change"
 		RunCourseLeafCgi "$stageInternal" "-r /support/tools/quicklinks"
-		Msg2 "Internal quicklinks page republished to reflect change"
+		Msg3 "Internal quicklinks page republished to reflect change"
 	else
-		Msg2 $W "Your account does not have access to the internal site file system, skipping client page republishing, please go to the client page on the internal site and republish the client page."
+		Msg3 $W "Your account does not have access to the internal site file system, skipping client page republishing, please go to the client page on the internal site and republish the client page."
 	fi
 else
-	Msg2 $W "The 'lfinternal' group was not defined in '/etc/group', skipping client page republishing, please go to the client page on the internal site and republish the client page."
+	Msg3 $W "The 'lfinternal' group was not defined in '/etc/group', skipping client page republishing, please go to the client page on the internal site and republish the client page."
 fi
 
 #===================================================================================================
@@ -188,3 +203,4 @@ Goodbye 0 #'alert'
 ## Wed Oct  5 09:46:17 CDT 2016 - dscudiero - Check to see if the user has access to the internal site befor rebuilding the client pages
 ## Mon Oct 17 08:48:30 CDT 2016 - dscudiero - Removed extra set of calls to rebuild pages
 ## 04-06-2017 @ 10.09.28 - (1.0.36)    - dscudiero - renamed RunCourseLeafCgi, use new name
+## 12-04-2017 @ 08.25.27 - (1.0.47)    - dscudiero - Updated to add arguments for parameters and switch to Msg3
