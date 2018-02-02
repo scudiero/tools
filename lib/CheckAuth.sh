84fb226ac3109da1e0e64e554a97c0fe45a96d34
David Scudiero <dscudiero@leepfrog.com>
2018-02-02 09:25:58 -0600

    Fix problem not seeing first token in the restrictToGroups string

--- lib/CheckAuth.sh
+++ lib/CheckAuth.sh
@@ -2,7 +2,7 @@
 
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.38" # -- dscudiero -- Fri 11/03/2017 @ 16:32:08.48
+# version="2.0.40" # -- dscudiero -- Fri 02/02/2018 @  9:23:16.84
 #===================================================================================================
 # Check to see if the logged user can run this script
 # Returns true if user is authorized, otherwise it returns a message
@@ -17,33 +17,54 @@ function CheckAuth {
 	local scriptName=${1-$myName}
 	unset author restrictGroups
 
-	[[ -z $UsersAuthGroups && -r "$TOOLSPATH/auth/$userName" ]] && UsersAuthGroups=$(cat "$TOOLSPATH/auth/$userName") || UsersAuthGroups='none'
+	[[ -z $UsersAuthGroups && -r "$TOOLSPATH/auth/$userName" ]] && UsersAuthGroups=$(cat "$TOOLSPATH/auth/$userName") || UsersAuthGroups='none' 
+
 	## Get the retricted information for the script
-		whereClauseUser="and (restrictToUsers like \"%$userName%\" or restrictToUsers is null)"
-		sqlStmt="select author,restrictToGroups from $scriptsTable where name=\"$scriptName\" $whereClauseUser"
+		sqlStmt="select author,restrictToUsers,restrictToGroups from $scriptsTable where name=\"$scriptName\""
 		RunSql2 $sqlStmt
 		[[ ${#resultSet[@]} -eq 0 ]] && echo true && return 0
 		result="${resultSet[0]}"
 		author="${result%%|*}"; result="${result#*|}";
-		[[ $author == $userName ]] && { echo true; return 0; }
-		restrictGroups="$result"
+		restrictToUsers="${result%%|*}"; result="${result#*|}";
+		restrictToGroups="$result"
+		#dump UsersAuthGroups author restrictToUsers restrictToGroups
 
-	## If there is restrictToGroups data then check
+	## If this is the author the let them run
+		[[ $author == $userName ]] && { echo true; return 0; }
+
+	## If there is restrictToUsers data then check
 		found=false
-		if [[ -n $restrictGroups && $restrictGroups != 'NULL' ]]; then
-			for group in ${UsersAuthGroups//,/ }; do
-				[[ $(Contains ",$restrictGroups," ",$group,") == true ]] && { echo true; return 0; }
+		if [[ -n $restrictToUsers && ${restrictToUsers^^[a-z]} != 'NULL' ]]; then
+			for restrictToUser in ${restrictToUsers//,/ }; do
+				[[ $restrictToUser == $userName ]] && found=true && break
 			done
-		else
-			echo true
-			return 0
 		fi
 
-	## User does not have access
-	echo "Sorry, you do not have permissions to run '$scriptName', the script is restricted to groups: '${restrictGroups//,/, }'.  FYI, you are in these groups: '${UsersAuthGroups//,/, }'"
+	## If there is restrictToGroups data then check
+		if [[ $found == false ]]; then
+			found=false
+			if [[ -n $restrictToGroups && ${restrictToGroups^^[a-z]} != 'NULL' ]]; then
+				for group in ${UsersAuthGroups//,/ }; do
+					for restrictToGroup in ${restrictToGroups//,/ }; do
+						[[ $restrictToGroup == $group ]] && found=true && break
+					done
+					[[ $found == true ]] && break
+				done
+			fi
+		fi
+
+	## User is authorized
+	if [[ $found == true ]]; then
+		echo true
+		return 0
+	fi
+
+	## User is not authorized
+	echo "Sorry, you do not have permissions to run '$scriptName', the script is restricted to groups: '${restrictToGroups//,/, }'.  FYI, you are in these groups: '${UsersAuthGroups//,/, }'"
 	return 0
 
 } #CheckAuth
+
 export -f CheckAuth
 #===================================================================================================
 # Checkin Log
@@ -60,3 +81,4 @@ export -f CheckAuth
 ## 09-29-2017 @ 13.30.34 - ("2.0.22")  - dscudiero - General syncing of dev to prod
 ## 10-03-2017 @ 14.36.14 - ("2.0.23")  - dscudiero - Remove all the UserAuthGroups stuff, moved to loader
 ## 11-06-2017 @ 07.22.41 - ("2.0.38")  - dscudiero - Switch to using the auth files
+## 02-02-2018 @ 09.25.58 - 2.0.40 - dscudiero - Fix problem not seeing first token in the restrictToGroups string
