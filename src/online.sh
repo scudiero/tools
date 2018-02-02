f920ce9d5d4d967dc9ff903abe56ef55365d35f7
David Scudiero <dscudiero@leepfrog.com>
2018-02-02 10:46:41 -0600

    D

--- src/online.sh
+++ src/online.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #===================================================================================================
-version=1.0.23 # -- dscudiero -- Tue 10/03/2017 @ 16:12:52.56
+version=1.0.24 # -- dscudiero -- Fri 02/02/2018 @ 10:46:26.38
 #===================================================================================================
 TrapSigs 'on'
 myIncludes="RunSql2"
@@ -13,6 +13,8 @@ scriptDescription="Bring a tools script online"
 ## turn a script offline -- i.e. create an .offline file
 #===================================================================================================
 GetDefaultsData $myName
+
+[[ $(Contains "$administrators" "$userName") != true ]] && Terminate "You do not have sufficient permissions to run this scrip"
 
 if [[ -n $originalArgStr ]]; then
 	for script in $originalArgStr; do
@@ -39,3 +41,4 @@ Goodbye
 ## Fri Jul 15 13:22:53 CDT 2016 - dscudiero - General syncing of dev to prod
 ## 10-03-2017 @ 07.39.17 - (1.0.13)    - dscudiero - Add RunSql2 to the include list
 ## 10-03-2017 @ 16.14.01 - (1.0.23)    - dscudiero - Refactored to allow for report of all online scripts
+## 02-02-2018 @ 10.46.41 - 1.0.24 - dscudiero - D
