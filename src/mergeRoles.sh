6df699cb483d3c903982e211b46666e0036c4102
David Scudiero <dscudiero@leepfrog.com>
2018-03-22 14:06:58 -0500

    Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2

--- src/mergeRoles.sh
+++ src/mergeRoles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #==================================================================================================
-version=2.2.48 # -- dscudiero -- Thu 11/02/2017 @ 11:01:24.21
+version=2.2.49 # -- dscudiero -- Thu 03/22/2018 @ 14:04:29.35
 #==================================================================================================
 TrapSigs 'on'
 myIncludes="GetOutputFile BackupCourseleafFile ProtectedCall SelectMenu GetExcel2 SetFileExpansion"
@@ -99,8 +99,8 @@ scriptDescription="Merge role data"
 		trap 'SignalHandler ERR ${LINENO} $? ${BASH_SOURCE[0]}' ERR
 		local rolesFile="$1"
 		#PushSettings; set +e; shift; PopSettings
-		[[ $useUINs == true && $processedUserData != true ]] && Msg3 $T "$FUNCNAME: Requesting UIN mapping but no userid sheet was provided"
-		Msg3 "\nParsing the roles.tcf file ($rolesFile) ..."
+		[[ $useUINs == true && $processedUserData != true ]] && Msg $T "$FUNCNAME: Requesting UIN mapping but no userid sheet was provided"
+		Msg "\nParsing the roles.tcf file ($rolesFile) ..."
 		## Get the roles data from the roles.tcf file
 			[[ ! -r $rolesFile ]] && Terminate "Could not read the .../courseleaf/roles.tcf file"
 			while read line; do
@@ -112,8 +112,8 @@ scriptDescription="Merge role data"
 		      	fi
 			done < $rolesFile
 
-			Msg3 "^Retrieved ${#rolesFromFile[@]} records"
-			if [[ $verboseLevel -ge 1 ]]; then Msg3 "\n^rolesFromFile: $roleFile"; for i in "${!rolesFromFile[@]}"; do printf "\t\t[$i] = >${rolesFromFile[$i]}<\n"; done; fi
+			Msg "^Retrieved ${#rolesFromFile[@]} records"
+			if [[ $verboseLevel -ge 1 ]]; then Msg "\n^rolesFromFile: $roleFile"; for i in "${!rolesFromFile[@]}"; do printf "\t\t[$i] = >${rolesFromFile[$i]}<\n"; done; fi
 		return 0
 	} #GetRolesDataFromFile
 
@@ -122,7 +122,7 @@ scriptDescription="Merge role data"
 	#==================================================================================================
 	function GetRolesDataFromSpreadsheet {
 		trap 'SignalHandler ERR ${LINENO} $? ${BASH_SOURCE[0]}' ERR
-		Msg3 "\nParsing the roles data from the workbook file ($workbookFile)..."
+		Msg "\nParsing the roles data from the workbook file ($workbookFile)..."
 		## Parse the role data from the spreadsheet
 			workbookSheet='roles'
 			GetExcel2 -wb "$workbookFile" -ws "$workbookSheet"
@@ -137,7 +137,7 @@ scriptDescription="Merge role data"
 					(( ii++ ))
 					result="${resultSet[$ii]}"
 					SetFileExpansion
-					Msg3 $V1 'Parsing header record'
+					Msg $V1 'Parsing header record'
 					IFSave=$IFS; IFS=\|; sheetCols=($result); IFS=$IFSave;
 					findFields="role members email"
 					for field in $findFields; do
@@ -173,8 +173,8 @@ scriptDescription="Merge role data"
 
 		[[ ${#rolesFromSpreadsheet[@]} -eq 0 ]] && Terminate "Did not retrieve any records from the spreadsheet, \
 					\n^most likely it is missing the 'start' directive ('*start*' or '***') in column 'A' just above the header record."
-		Msg3 "^Retrieved ${#rolesFromSpreadsheet[@]} records"
-		if [[ $verboseLevel -ge 1 ]]; then Msg3 "\n^rolesFromSpreadsheet: $roleFile"; for i in "${!rolesFromSpreadsheet[@]}"; do printf "\t\t[$i] = >${rolesFromSpreadsheet[$i]}<\n"; done; fi
+		Msg "^Retrieved ${#rolesFromSpreadsheet[@]} records"
+		if [[ $verboseLevel -ge 1 ]]; then Msg "\n^rolesFromSpreadsheet: $roleFile"; for i in "${!rolesFromSpreadsheet[@]}"; do printf "\t\t[$i] = >${rolesFromSpreadsheet[$i]}<\n"; done; fi
 
 		return 0
 
@@ -241,7 +241,7 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 	[[ $merge == true ]] && mergeMode='merge'
 	if [[ $mergeMode == '' && $informationOnlyMode != true ]]; then
 		unset ans
-		Msg3 "\n Do you wish to merge data when roles are found in both source and target, or do you want to overlay the target data from the source"
+		Msg "\n Do you wish to merge data when roles are found in both source and target, or do you want to overlay the target data from the source"
 		Prompt ans "'Yes' = 'merge', 'No' = 'overlay'" 'Yes No' 'Yes'; ans=$(Lower ${ans:0:1})
 		[[ $ans == 'y' ]] && mergeMode='merge' || mergeMode='overlay'
 	else
@@ -251,10 +251,10 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 ## Find out if this client uses UINs
 	if [[ $noUinMap == false ]]; then
 		sqlStmt="select usesUINs from $clientInfoTable where name=\"$client\""
-			RunSql2 $sqlStmt
+			RunSql $sqlStmt
 			if [[ ${#resultSet[@]} -ne 0 ]]; then
 				result="${resultSet[0]}"
-				[[ $result == 'Y' ]] && useUINs=true && Msg3
+				[[ $result == 'Y' ]] && useUINs=true && Msg
 			fi
 	fi
 
@@ -263,7 +263,7 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 	if [[ $workbookFile == '' && $verify != false ]]; then
 		if [[ $verify != false ]]; then
 			unset ans
-			Msg3
+			Msg
 			Prompt ans "Do you wish to merge in spreadsheet data" 'Yes No' 'No'; ans=$(Lower ${ans:0:1})
 			if [[ $ans == 'y' ]]; then
 				## Search for XLSx files in clientData and implimentation folders
@@ -297,7 +297,7 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 					else
 						Info "No .xlsx files found"
 					fi
-				[[ $workbookFile == "" ]] && Msg3
+				[[ $workbookFile == "" ]] && Msg
 				Prompt workbookFile 'Please specify the full path to the workbook file' '*file*'
 				fi
 			fi
@@ -352,7 +352,7 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 		## Make sure we have a 'role' sheet
 			[[ $(Contains "$(Lower $sheets)" 'role') != true ]] && Terminate"Could not locate a sheet with 'role' in its name in workbook:\n^$workbookFile"
 			GetRolesDataFromSpreadsheet
-			if [[ $verboseLevel -ge 1 ]]; then Msg3 "\n^rolesfromSpreadsheet:"; for i in "${!rolesFromSpreadsheet[@]}"; do printf "\t\t[$i] = >${rolesFromSpreadsheet[$i]}<\n"; done; fi
+			if [[ $verboseLevel -ge 1 ]]; then Msg "\n^rolesfromSpreadsheet:"; for i in "${!rolesFromSpreadsheet[@]}"; do printf "\t\t[$i] = >${rolesFromSpreadsheet[$i]}<\n"; done; fi
 	fi
 
 ## Process role.tcf files
@@ -369,7 +369,7 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 	unset rolesFromFile
 
 ## Merge role file data
-	Msg3 "\nMerging the '$srcEnv' roles into the '$tgtEnv' roles ..."
+	Msg "\nMerging the '$srcEnv' roles into the '$tgtEnv' roles ..."
 	unset numDifferentFromSrc addedFromSrc
 	## Prime roles out array with the tgt file data.
 	for key in "${!rolesFromTgtFile[@]}"; do rolesOut["$key"]="${rolesFromTgtFile["$key"]}"; done
@@ -377,8 +377,8 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 		if [[ ${rolesOut["$key"]+abc} ]]; then
 			if [[ ${rolesFromSrcFile["$key"]} != ${rolesOut["$key"]} ]]; then
 				Warning  0 1 "Role '$key' data in '$srcEnv' differs from '$tgtEnv'"
-				Msg3 "^^$srcEnv data: ${rolesFromSrcFile["$key"]}"
-				Msg3 "^^$tgtEnv data: ${rolesOut["$key"]}"
+				Msg "^^$srcEnv data: ${rolesFromSrcFile["$key"]}"
+				Msg "^^$tgtEnv data: ${rolesOut["$key"]}"
 				if [[ $mergeMode == 'merge' ]]; then
 					unset members1 members2 email1 email2 mergedMembers mergedEmail fromEnv
 					rolesOut["//$key"]="${rolesFromSrcFile["$key"]}   <-- Pre-merge $srcEnv value"
@@ -391,18 +391,18 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 					if [[ $email1 != $email2 ]]; then
 						[[ $email1 != '' ]] && mergedEmail="$email1" && fromEnv="$srcEnv"
 						[[ $email2 != '' ]] && mergedEmail="$email2" && fromEnv="$tgtEnv"
-						Msg3 "^^Email data on the roles do not match, using: '$mergedEmail' from '$fromEnv'"
+						Msg "^^Email data on the roles do not match, using: '$mergedEmail' from '$fromEnv'"
 						#warningMsgs+=("\tEmail data on the roles do not match, using: '$mergedEmail' from '$fromEnv'")
 					else
 						mergedEmail="$email2"
 					fi
 					dump -1 -t -t members1 members2 mergedMembers email1 email2 mergedEmail
 					rolesOut["$key"]="$mergedMembers|$mergedEmail"
-					Msg3 "^^New $tgtEnv (merged) data: ${rolesOut["$key"]}"
+					Msg "^^New $tgtEnv (merged) data: ${rolesOut["$key"]}"
 					#warningMsgs+=("\tNew $tgtEnv (merged) data: ${rolesOut["$key"]}")
 				else
 					rolesOut["//$key"]="${rolesFromSrcFile["$key"]}   <-- Pre-merge $srcEnv value"
-					Msg3 "^^Keeping existing ($tgtEnv) data."
+					Msg "^^Keeping existing ($tgtEnv) data."
 				fi
 				(( numDifferentFromSrc += 1 ))
 			fi
@@ -413,22 +413,22 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 	done;
 
 	numRolesOut=${#rolesOut[@]}
-	Msg3 "^Merged roles out record count: ${numRolesOut}"
-	[[ $addedFromSrc -gt 0 ]] && Msg3 "^$addedFromSrc records added from '$srcEnv'"
-	[[ $numDifferentFromSrc -gt 0 ]] && Msg3 "^$numDifferentFromSrc records differed between '$srcEnv' and '$tgtEnv'"
+	Msg "^Merged roles out record count: ${numRolesOut}"
+	[[ $addedFromSrc -gt 0 ]] && Msg "^$addedFromSrc records added from '$srcEnv'"
+	[[ $numDifferentFromSrc -gt 0 ]] && Msg "^$numDifferentFromSrc records differed between '$srcEnv' and '$tgtEnv'"
 
-	if [[ $verboseLevel -ge 1 ]]; then Msg3 "\n^rolesOut: "; for i in "${!rolesOut[@]}"; do printf "\t\t[$i] = >${rolesOut[$i]}<\n"; done; fi
+	if [[ $verboseLevel -ge 1 ]]; then Msg "\n^rolesOut: "; for i in "${!rolesOut[@]}"; do printf "\t\t[$i] = >${rolesOut[$i]}<\n"; done; fi
 
 ## Merge role spreadsheet data
 	if [[ $workbookFile != '' ]]; then
 		unset numDifferentFromSpreadsheet addedFromSpreadsheet
-		Msg3 "\nMerging the spreadsheet role data into the '$tgtEnv' roles ..."
+		Msg "\nMerging the spreadsheet role data into the '$tgtEnv' roles ..."
 		for key in "${!rolesFromSpreadsheet[@]}"; do
 			if [[ ${rolesOut["$key"]+abc} ]]; then
 				if [[ ${rolesFromSpreadsheet["$key"]} != ${rolesOut["$key"]} ]]; then
 					Warning 0 1 "Role '$key' data in spreadsheet differs from '$tgtEnv'"
-					Msg3 "^^Spreadsheet data: ${rolesFromSpreadsheet["$key"]}"
-					Msg3 "^^$tgtEnv data: ${rolesOut["$key"]}"
+					Msg "^^Spreadsheet data: ${rolesFromSpreadsheet["$key"]}"
+					Msg "^^$tgtEnv data: ${rolesOut["$key"]}"
 					if [[ $mergeMode == 'merge' ]]; then
 						unset members1 members2 email1 email2 mergedMembers mergedEmail
 						rolesOut["//$key"]="${rolesFromSpreadsheet["$key"]}   <-- Pre-merge spreadsheet value"
@@ -441,18 +441,18 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 						if [[ $email1 != $email2 ]]; then
 						[[ $email2 != '' ]] && mergedEmail="$email2" && fromEnv="$tgtEnv"
 						[[ $email1 != '' ]] && mergedEmail="$email1" && fromEnv="spreadsheet"
-						Msg3 "^^Email data on the roles do not match, using: '$mergedEmail' from '$fromEnv'"
+						Msg "^^Email data on the roles do not match, using: '$mergedEmail' from '$fromEnv'"
 						#warningMsgs+=("\tEmail data on the roles do not match, using: '$mergedEmail' from '$fromEnv'")
 						else
 							mergedEmail="$email2"
 						fi
 						dump -1 -t -t members1 members2 mergedMembers email1 email2 mergedEmail
 						rolesOut["$key"]="$mergedMembers|$mergedEmail"
-						Msg3 "^^New $tgtEnv (merged) data: ${rolesOut["$key"]}"
+						Msg "^^New $tgtEnv (merged) data: ${rolesOut["$key"]}"
 						#warningMsgs+=("^New $tgtEnv (merged) data: ${rolesOut["$key"]}")
 					else
 						rolesOut["//$key"]="${rolesFromSpreadsheet["$key"]}   <-- Pre-merge spreadsheet value"
-						Msg3 "^^Keeping existing ($tgtEnv) data."
+						Msg "^^Keeping existing ($tgtEnv) data."
 					fi
 					(( numDifferentFromSrc += 1 ))
 				fi
@@ -462,9 +462,9 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 			fi
 		done
 
-		Msg3 "^Merged roles out record count: ${#rolesOut[@]}"
-		[[ $addedFromSpreadsheet -gt 0 ]] && Msg3 "^$addedFromSpreadsheet records added from the Spreadsheet"
-		[[ $numDifferentFromSpreadsheet -gt 0 ]] && Msg3 "^$numDifferentFromSpreadsheet records differed between Spreadsheet and '$tgtEnv'"
+		Msg "^Merged roles out record count: ${#rolesOut[@]}"
+		[[ $addedFromSpreadsheet -gt 0 ]] && Msg "^$addedFromSpreadsheet records added from the Spreadsheet"
+		[[ $numDifferentFromSpreadsheet -gt 0 ]] && Msg "^$numDifferentFromSpreadsheet records differed between Spreadsheet and '$tgtEnv'"
 	fi
 
 ## Write out file
@@ -472,13 +472,13 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 	if [[ $informationOnlyMode != true ]]; then
 		writeFile=true
 		if [[ ${#warningMsgs[@]} -gt 0 ]]; then
-			Msg3; unset ans
+			Msg; unset ans
 			Prompt ans "Warning messages were issued, do you wish to write the role data out to '$tgtEnv'" "Yes No"; ans=$(Lower ${ans:0:1})
 			[[ $ans != 'y' ]] && writeFile=false
 		fi
 		if [[ $writeFile == true ]]; then
 			rolesFile=$tgtDir/web/courseleaf/roles.tcf
-			Msg3 "\nWriting out new roles.tcf file to '$tgtEnv' ($rolesFile)..."
+			Msg "\nWriting out new roles.tcf file to '$tgtEnv' ($rolesFile)..."
 			editFile=$rolesFile
 			BackupCourseleafFile $editFile
 			# Parse the target file to put source data in the correct location in target file.
@@ -502,12 +502,12 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 			## Swap the files
 				mv $editFile.new $editFile
 				[[ -f $topPart ]] && rm -f $topPart; [[ -f $bottomPart ]] && rm -f $bottomPart
-			Msg3 "^$editFile written to disk"
+			Msg "^$editFile written to disk"
 			summaryMsgs+=("$editFile written to disk")
 			## Write out change log entries
-				$DOIT Msg3 "\n$userName\t$(date) via '$myName' version: $version" >> $tgtDir/changelog.txt
-				$DOIT Msg3 "^Merged data from '$srcEnv'" >> $tgtDir/changelog.txt
-				[[ $workbookFile != '' ]] && $DOIT Msg3 "^Merged data from $realWorkbookFile" >> $tgtDir/changelog.txt
+				$DOIT Msg "\n$userName\t$(date) via '$myName' version: $version" >> $tgtDir/changelog.txt
+				$DOIT Msg "^Merged data from '$srcEnv'" >> $tgtDir/changelog.txt
+				[[ $workbookFile != '' ]] && $DOIT Msg "^Merged data from $realWorkbookFile" >> $tgtDir/changelog.txt
 		fi # writeFile
 	fi #not informationOnlyMode
 
@@ -544,7 +544,8 @@ tgtEnv="$(TitleCase "$tgtEnv")"
 ## Tue Sep 20 12:34:42 CDT 2016 - dscudiero - Switched to use Msg2
 ## 05-26-2017 @ 12.49.39 - (2.2.21)    - dscudiero - Found an instance of Msg vs Msg2
 ## 06-07-2017 @ 07.44.14 - (2.2.22)    - dscudiero - Added BackupCourseleafFIle to the import list
-## 09-25-2017 @ 12.26.53 - (2.2.24)    - dscudiero - Switch to use Msg3
+## 09-25-2017 @ 12.26.53 - (2.2.24)    - dscudiero - Switch to use Msg
 ## 10-16-2017 @ 09.06.27 - (2.2.45)    - dscudiero - Updated to use GetExcel2
 ## 11-02-2017 @ 06.58.56 - (2.2.47)    - dscudiero - Switch to ParseArgsStd2
 ## 11-02-2017 @ 11.02.12 - (2.2.48)    - dscudiero - Add addPvt to the init call
+## 03-22-2018 @ 14:06:58 - 2.2.49 - dscudiero - Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2
