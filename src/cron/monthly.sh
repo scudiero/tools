d70a920ee6733836e613bf2fd2a5f53aab685a7a
David Scudiero <dscudiero@leepfrog.com>
2018-03-22 12:47:08 -0500

    Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2

--- src/cron/monthly.sh
+++ src/cron/monthly.sh
@@ -1,12 +1,13 @@
 #=======================================================================================================================
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version=2.1.12 # -- dscudiero -- Mon 12/18/2017 @  7:50:12.48
+version=2.1.13 # -- dscudiero -- Thu 03/22/2018 @ 12:44:07.86
 #=======================================================================================================================
 # Run once a week from cron
 #=======================================================================================================================
 TrapSigs 'on'
-Import GetDefaultsData ParseArgsStd2 ParseArgs Msg3
+Import "$standardIncludes $myIncludes"
+
 originalArgStr="$*"
 
 #=======================================================================================================================
@@ -27,13 +28,13 @@ case "$hostName" in
 	mojave)
 			## Roll up the weekly log archives
 				[[ $verbose == true ]] && printf "\n$(PadChar '-')\n" && printf "*** Log file roll up ***\n"
-				Msg3 "\n*** Logs rollup -- Starting ***"
+				Msg "\n*** Logs rollup -- Starting ***"
 				lastMonthNum=$(date --date="$(date +%Y-%m-15) -1 month" +'%m')
 				lastMonthAbbrev=$(date --date="$(date +%Y-%m-15) -1 month" +'%b')
 				cd $TOOLSPATH/Logs
 				[[ -d ./cronJobs ]] && rm -rf ./cronJobs
 				tar -cvzf "${lastMonthAbbrev}-$(date '+%Y').tar.gz" $lastMonthNum-* --remove-files
-				Msg3 "\n^$myName logs rollup -- Completed ***"
+				Msg "\n^$myName logs rollup -- Completed ***"
 			;;
 	build5)
 			;;
@@ -53,3 +54,4 @@ return 0
 ## 10-11-2017 @ 10.37.45 - (2.1.9)     - dscudiero - Switch to use FindExecutable -run
 ## 11-22-2017 @ 06.28.33 - (2.1.10)    - dscudiero - switch to use parsargsstd2
 ## 12-18-2017 @ 07.50.50 - (2.1.12)    - dscudiero - Tweak messaging
+## 03-22-2018 @ 12:47:08 - 2.1.13 - dscudiero - Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2
