629b22c38a8aad01461f5688b49653b66d2bacf1
David Scudiero <dscudiero@leepfrog.com>
2018-03-22 13:16:49 -0500

    Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2

--- lib/Semaphore.sh
+++ lib/Semaphore.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.25" # -- dscudiero -- Fri 10/20/2017 @ 12:41:53.17
+# version="2.0.26" # -- dscudiero -- Thu 03/22/2018 @ 13:11:08.01
 #===================================================================================================
 # Process semaphores
 # Semaphore <mode> <key/name> <sleeptime>
@@ -14,7 +14,7 @@
 # All rights reserved
 #===================================================================================================
 function Semaphore {
-	myIncludes="StringFunctions RunSql2"
+	myIncludes="StringFunctions RunSql"
 	Import "$myIncludes"
 
 	local mode=${1:-'check'}
@@ -31,14 +31,14 @@ function Semaphore {
 	if [[ ${mode:0:1} == 's' ]]; then  ## 'set'
 		sqlStmt="insert into $semaphoreInfoTable (keyId,processName,hostName,createdBy,createdOn) \
 						 values(NULL,\"$myName\",\"$hostName\",\"$userName\",\"$startTime\")";
-		RunSql2 $sqlStmt
+		RunSql $sqlStmt
 		sqlStmt="select max(keyId) from $semaphoreInfoTable"
-		RunSql2 $sqlStmt
+		RunSql $sqlStmt
 		echo ${resultSet[0]}
 
 	elif [[ ${mode:0:1} == 'g' ]]; then  ## 'getkeys'
 		sqlStmt="select keyId from $semaphoreInfoTable where processName=\"$keyId\" $andClause";
-		RunSql2 $sqlStmt
+		RunSql $sqlStmt
  		if [[ ${#resultSet[@]} -ne 0 ]]; then
  			local retString
  			for result in ${resultSet[@]}; do
@@ -49,12 +49,12 @@ function Semaphore {
 
 	elif [[ ${mode:0:2} == 'ch' ]]; then  ## 'check'
 		sqlStmt="select count(*) from $semaphoreInfoTable where processName=\"$keyId\" $andClause";
-		RunSql2 $sqlStmt
+		RunSql $sqlStmt
  		[[ ${resultSet[0]} -ne 0 ]] && echo false || echo true
 
 	elif [[ ${mode:0:2} == 'cl' ]]; then  ## 'clear'
 		sqlStmt="delete from $semaphoreInfoTable where keyId=\"$keyId\"";
-		RunSql2 $sqlStmt
+		RunSql $sqlStmt
 
 	elif [[ ${mode:0:1} == 'w' ]]; then  ## 'waiton'
 		count=1
@@ -62,7 +62,7 @@ function Semaphore {
 		while [[ $count -gt 0 ]]; do
 			[[ $(IsNumeric $keyId) == true ]] && whereClause="keyId=\"$keyId\"" || whereClause="processName=\"$keyId\""
 			sqlStmt="select count(*) from $semaphoreInfoTable where $whereClause";
-			RunSql2 $sqlStmt
+			RunSql $sqlStmt
  			[[ ${#resultSet[@]} -ne 0 ]] && count=${resultSet[0]} || count=0
  			[[ $count -gt 0 ]] && dump -3 -l -t count && ((waitCnt++)) && sleep $sleepTime
  			[[ $waitCnt -gt $timeOutCnt ]] && Terminate "$FUNCNAME - Wait action timed out waiting on '$whereClause' after $timeOutCnt iterations."
@@ -84,7 +84,7 @@ function CheckSemaphore {
 	unset okToRun okToRunWaiton
 	okToRun=$(Semaphore 'check' $callPgmName)
 	## Check to see if we are running or any waitOn process are running
-	[[ $okToRun == false && $(Contains ",$waitOn," ',self,') != true ]] && Msg2 $T "CallPgm: Another instance of this script ($callPgmName) is currently running.\n"
+	[[ $okToRun == false && $(Contains ",$waitOn," ',self,') != true ]] && Terminate "CallPgm: Another instance of this script ($callPgmName) is currently running.\n"
 	if [[ $waitOn != '' ]]; then
 		for pName in $(echo $waitOn | tr ',' ' '); do
 			waitonMode=$(cut -d':' -f2 <<< $pName)
@@ -92,7 +92,7 @@ function CheckSemaphore {
 			[[ $(Lower $waitonMode) == 'g' ]] && checkAllHosts='checkAllHosts' || unset checkAllHosts
 			okToRun=$(Semaphore 'check' $pName $checkAllHosts)
 			if [[ $okToRun == false ]]; then
-				[[ $batchMode != true ]] && Msg2 "CallPgm: Waiting for process '$pName' to finish..."
+				[[ $batchMode != true ]] && Msg "CallPgm: Waiting for process '$pName' to finish..."
 				Semaphore 'waiton' "$pName" $checkAllHosts
 			fi
 		done
@@ -114,3 +114,4 @@ export -f CheckSemaphore
 ## Tue Feb 14 09:44:10 CST 2017 - dscudiero - Added 'getKey' action to retrieve keys
 ## Tue Feb 14 10:47:30 CST 2017 - dscudiero - Added time out counter for waiton action
 ## 10-20-2017 @ 12.42.16 - ("2.0.25")  - dscudiero - Add StringFunctions to the included list
+## 03-22-2018 @ 13:16:48 - 2.0.26 - dscudiero - Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2
