a0752806a6aa0a2623c196698976a59d16373432
David Scudiero <dscudiero@leepfrog.com>
2018-03-22 14:07:27 -0500

    Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2

--- src/reports/clientDataForLUC.sh
+++ src/reports/clientDataForLUC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #XO NOT AUTOVERSION
-version=1.0.26 # -- dscudiero -- Thu 01/18/2018 @ 15:40:46.18
+version=1.0.29 # -- dscudiero -- Thu 03/22/2018 @ 13:58:59.09
 originalArgStr="$*"
 scriptDescription=""
 TrapSigs 'on'
@@ -11,18 +11,6 @@ TrapSigs 'on'
 #==================================================================================================
 # Standard call back functions
 #==================================================================================================
-function parseArgs-clientDataForLUC  { # or parseArgs-local
-	:
-	return 0
-}
-function Goodbye-clientDataForLUC  { # or Goodbye-local
-	SetFileExpansion 'on' ; rm -rf $tmpRoot/${myName}* >& /dev/null ; SetFileExpansion
-	return 0
-}
-function testMode-clientDataForLUC  { # or testMode-local
-	[[ $userName != 'dscudiero' ]] && Msg "T You do not have sufficient permissions to run this script in 'testMode'"
-	return 0
-}
 
 #==================================================================================================
 # Declare local variables and constants
@@ -39,9 +27,9 @@ ParseArgsStd2 $originalArgStr
 #==================================================================================================
 fields="name,longname,primarycontact,salesRep,catSup,cimSup,catCsm,cimCsm,clssCsm"
 sqlStmt="select $fields from $clientInfoTable where recordstatus=\"A\" order by name"
-RunSql2 $sqlStmt
+RunSql $sqlStmt
 if [[ ${#resultSet[@]} -gt 0 ]]; then
-	Msg3 "${fields//,/\t}"
+	Msg "${fields//,/\t}"
 	for result in "${resultSet[@]}"; do
 		## Escape single and double quotes
 		result="${result//\'/''}"; result="${result//\"/""}"; result="${result//NULL/ }"
@@ -53,7 +41,7 @@ if [[ ${#resultSet[@]} -gt 0 ]]; then
 			outLine="$outLine\t${!field}"
 			((fieldCntr += 1))
 		done
-		Msg3 "${outLine:2}"
+		Msg "${outLine:2}"
 	done
 else
 	Warning "Did not find any contacts records meeting criteria"
@@ -71,3 +59,4 @@ Goodbye 'quiet'
 ## 11-06-2017 @ 16.43.26 - (1.0.12)    - dscudiero - Switch to new excel reader
 ## 11-08-2017 @ 12.22.21 - (1.0.13)    - dscudiero - Only return clients who have 'opted-in' (leepday=Y)
 ## 11-15-2017 @ 11.25.47 - (1.0.17)    - dscudiero - Updated to only report opted in contact records
+## 03-22-2018 @ 14:07:27 - 1.0.29 - dscudiero - Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2
