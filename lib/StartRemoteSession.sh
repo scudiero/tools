f22d7da0947db741d5d899584bb3e2403adb92d4
David Scudiero <dscudiero@leepfrog.com>
2018-03-26 13:00:54 -0500

    Removing file

--- lib/StartRemoteSession.sh
+++ lib/StartRemoteSession.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-#version="2.0.14" # -- dscudiero -- Fri 03/23/2018 @ 16:55:25.14
+#version="3.0.0" # -- dscudiero -- Mon 03/26/2018 @  9:10:27.55
 #===================================================================================================
 # Start a remote session via ssh
 # StartRemoteSession userid@domain [command]
@@ -10,35 +10,9 @@
 #===================================================================================================
 
 function StartRemoteSession {
-	myIncludes="ProtectedCall StringFunctions"
-	Import "$standardIncludes $myIncludes"
-
-	local remoteUserAtHost=$1; shift
-	local remoteCommand="$*"
-
-	local commmandPrefix pwRec lookupKey tokens remoteHost remoteUser remotePw remoteDomain
-	unset pwRec
-
-	remoteUser=$(cut -d '@' -f1 <<< $remoteUserAtHost)
-	remoteDomain=$(cut -d '@' -f2 <<< $remoteUserAtHost)
-	remoteHost=$(cut -d '.' -f1 <<< $remoteDomain)
-	lookupKey=$remoteHost
-
-	#E Does the user have sshpass and a .pw2 file in their home directory
-	local pwFile=$HOME/.pw2
-	whichOut=$(ProtectedCall "which sshpass 2> /dev/null")
-	if [[ $whichOut != '' && -r $pwFile && $lookupKey != '' ]]; then
-		pwRec=$(grep "^$lookupKey" $pwFile)
-		if [[ $pwRec != '' ]]; then ## [0]=key, [1]=userid, [2]=password, [3]=remoteHost
-			read -ra tokens <<< "$pwRec"
-			[[ ${tokens[3]} != '' ]] && remoteUserAtHost="${tokens[1]}@${tokens[3]}" || remoteUserAtHost="${tokens[1]}@${remoteDomain}"
-			commmandPrefix="sshpass -p ${tokens[2]}"
-		fi
-	fi
-
-	[[ $(Contains "$remoteUserAtHost" '.') == false ]] && remoteUserAtHost="${remoteUserAtHost}.leepfrog.com"
-	#[[ $(Contains ",$slowHosts," ",$remoteHost,") == true ]] && Note "Target host has been found to be a bit slow, please be patient" && Msg
-	$commmandPrefix ssh -t $remoteUserAtHost $DISPATCHER $remoteCommand
+	local host=$1
+	local jar="$TOOLSPATH/jars/myPutty.jar"
+	java -jar $jar -s $host
 	return 0
 } ## StartRemoteSession
 export -f StartRemoteSession
