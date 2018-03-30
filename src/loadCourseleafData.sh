6b91c98ba420881edefb45407f7ba3e7b31bf8fe
David Scudiero <dscudiero@leepfrog.com>
2018-03-30 10:00:10 -0500

    Add checks to make sure there is a valid worksheet in the workbook if the user has specified arguments

--- src/loadCourseleafData.sh
+++ src/loadCourseleafData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # XO NOT AUTOVERSION
 #==================================================================================================
-version=3.9.21 # -- dscudiero -- Fri 03/30/2018 @  9:48:23.93
+version=3.9.25 # -- dscudiero -- Fri 03/30/2018 @  9:59:09.66
 #==================================================================================================
 TrapSigs 'on'
 myIncludes='DbLog Prompt SelectFile VerifyContinue InitializeInterpreterRuntime GetExcel WriteChangelogEntry'
@@ -817,13 +817,13 @@ dump -1 processUserData processRoleData processPageData informationOnlyMode igno
 	IFS='|' read -ra sheets <<< "${resultSet[0]}"
 	unset usersSheet rolesSheet pagesSheet
 	for sheet in "${sheets[@]}"; do
-		[[ $(Contains "$(Lower $sheet)" 'user') == true ]] && usersSheet="$sheet"
-		[[ $(Contains "$(Lower $sheet)" 'role') == true ]] && rolesSheet="$sheet"
-		[[ $(Contains "$(Lower $sheet)" 'workflow') == true ]] && pagesSheet="$sheet"
+		[[ $(Contains "${sheet,,[a-z]}" 'user') == true ]] && usersSheet="$sheet"
+		[[ $(Contains "${sheet,,[a-z]}" 'role') == true ]] && rolesSheet="$sheet"
+		[[ $(Contains "${sheet,,[a-z]}" 'workflow') == true ]] && pagesSheet="$sheet"
 	done
 
 [[ $processUserData == true || $processRoleData == true || $processPageData == true ]] && dataArgSpecified=true || dataArgSpecified=false
-dump -1 processUserData processRoleData processPageData dataArgSpecified
+dump -1 processUserData processRoleData processPageData dataArgSpecified usersSheet rolesSheet pagesSheet
 ## What data should we load
 	if [[ $dataArgSpecified != true ]]; then
 		if [[ $verify == true && $processUserData != true && -n $usersSheet ]]; then
@@ -843,6 +843,11 @@ dump -1 processUserData processRoleData processPageData dataArgSpecified
 			[[ $ans == 'y' ]] && processPageData=true
 			[[ $ans == 'a' ]] && processUserData=true && processRoleData=true && processPageData=true
 		fi
+	else
+		[[ $processUserData == true && -z $usersSheet ]] && Terminate "Sorry, you specified '-users' on the command line but could not locate a 'Users' worksheet."
+		[[ $processRoleData == true && -z $rolesSheet ]] && Terminate "Sorry, you specified '-roles' on the command line but could not locate a 'Roles' worksheet."
+		[[ $processPageData == true && -z $pagesSheet ]] && Terminate "Sorry, you specified '-pages' on the command line but could not locate a 'Workflow' worksheet."
+
 	fi
 
 	# ## If we are processing page data and role data see if the user page has uin mapping information
@@ -1114,3 +1119,4 @@ ignoreMissingPages=true
 ## 03-29-2018 @ 08:39:42 - 3.9.17 - dscudiero - Fix WarningMsg, false role data messaging
 ## 03-30-2018 @ 09:39:47 - 3.9.18 - dscudiero - Comment out dataArgSpecified check, allow prompts
 ## 03-30-2018 @ 09:48:57 - 3.9.21 - dscudiero - Fix problem where we were not prompting for what data to load
+## 03-30-2018 @ 10:00:10 - 3.9.25 - dscudiero - Add checks to make sure there is a valid worksheet in the workbook if the user has specified arguments
