faacf60b370ded188652f02be52d49f178ab3a97
David Scudiero <dscudiero@leepfrog.com>
2018-06-05 15:21:23 -0500

    (1.0.28) Cosmetic/minor change/Sync

--- src/online.sh
+++ src/online.sh
@@ -18,8 +18,7 @@ GetDefaultsData $myName
 
 if [[ -n $originalArgStr ]]; then
 	for script in $originalArgStr; do
-		[[ -f "$TOOLSPATH/bin/${script%.*}.offline" ]] && rm -f "$TOOLSPATH/bin/${script%.*}.offline"
-		Msg "^$script is now on-line"
+		[[ -f "$TOOLSPATH/bin/${script%.*}.offline" ]] && { rm -f "$TOOLSPATH/bin/${script%.*}.offline"; Msg "^$script is now on-line"; }
 	done
 fi
 
@@ -33,21 +32,6 @@ Goodbye
 ## 10-03-2017 @ 16.14.01 - (1.0.23)    - dscudiero - Refactored to allow for report of all online scripts
 ## 02-02-2018 @ 10.46.41 - 1.0.24 - dscudiero - D
 ## 03-22-2018 @ 14:07:12 - 1.0.26 - dscudiero - Updated for Msg3/Msg, RunSql2/RunSql, ParseArgStd/ParseArgStd2
-
-
-if [[ -n $originalArgStr ]]; then
-	for script in $originalArgStr; do
-		[[ -f "$TOOLSPATH/bin/${script%.*}.offline" ]] && rm -f "$TOOLSPATH/bin/${script%.*}.offline"
-		Msg "^$script is now offline"
-	done
-else
-	Msg "Current offline scripts:"
-	files=("$(ls $TOOLSPATH/bin/*.offline)")
-	for file in "${files[@]}"; do
-		file="${file%.*}"
-		Msg "^${file##*/}"
-	done
-	Msg
-fi
 ## 05-25-2018 @ 09:14:19 - 1.0.28 - dscudiero - Re-factor to use .offline files
 ## 06-05-2018 @ 15:14:41 - 1.0.28 - dscudiero - Cosmetic/minor change/Sync
+## 06-05-2018 @ 15:21:23 - 1.0.28 - dscudiero - Cosmetic/minor change/Sync
