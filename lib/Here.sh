b6c15904c90df30ffee1ecfe898c4d3a9206c36a
David Scudiero <dscudiero@leepfrog.com>
2018-06-15 08:31:50 -0500

    (2.0.13) Allow echo to edit the string

--- lib/Here.sh
+++ lib/Here.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="2.0.12" # -- dscudiero -- Wed 11/15/2017 @  8:31:02.09
+version="2.0.13" # -- dscudiero -- Fri 06/15/2018 @ 08:31:27
 #===================================================================================================
 # Got HERE
 #===================================================================================================
@@ -12,15 +12,15 @@ function Here {
 
 	if [[ $* == '' ]]; then
 		local callerInfo=($(caller))
-		echo "HERE: $(basename ${callerInfo[1]}), line: ${callerInfo[0]}"
+		echo -e "HERE: $(basename ${callerInfo[1]}), line: ${callerInfo[0]}"
 		return
 	fi
 
 	if [[ $1 == '-s' && -n $stdout ]]; then
 		shift || true
-		echo HERE: $* >> $stdout
+		echo -e HERE: $* >> $stdout
 	else
-		echo HERE: $*
+		echo -e HERE: $*
 	fi
 
 	return 0
@@ -37,3 +37,4 @@ export -f here
 ## Wed Jan  4 13:53:43 CST 2017 - dscudiero - General syncing of dev to prod
 ## 11-15-2017 @ 08.29.15 - ("2.0.10")  - dscudiero - Added -s option to write to $stdout
 ## 11-15-2017 @ 08.33.18 - ("2.0.12")  - dscudiero - Cosmetic/minor change
+## 06-15-2018 @ 08:31:50 - 2.0.13 - dscudiero - Allow echo to edit the string
