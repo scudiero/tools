dabb43c7efeba21cfc19083934471a3fe3aba230
David Scudiero <dscudiero@leepfrog.com>
2018-07-02 13:32:59 -0500

    (1.0.-1) Add data checks for the 'currentRecord' records

--- src/loadPatchData.sh
+++ src/loadPatchData.sh
@@ -8,7 +8,7 @@ version=1.0.-1 # -- dscudiero -- 10/20/2016 @ 14:58:14.98
 #
 #=======================================================================================================================
 TrapSigs 'on'
-myIncludes="DatabaseUtilities SetFileExpansion"
+myIncludes="DatabaseUtilities SetFileExpansion PushPop"
 Import "$standardInteractiveIncludes $myIncludes"
 
 originalArgStr="$*"
@@ -63,7 +63,7 @@ user="$client"
 
 if [[ $batchMode != true ]]; then
 	unset ans
-	Prompt ans "You are asking to reload the courseleafPatch control data, do you wish to continue" 'Yes No';
+	Prompt ans "You are asking to reload the courseleafPatch control data, do you wish to continue" 'Yes No' 'Yes';
 	ans="${ans:0:1}"; ans="${ans,,[a-z]}"
 	[[ $ans != 'y' ]] && Goodbye 3
 fi
@@ -78,20 +78,42 @@ fi
 	dbFile="${internalSiteRoot}/stage${grepStr##*|}"
 	getTableColumns "$patchesTable" 'warehouse' 'numFields' 'fields'
 
-## Make a copy of the warehouse table 
-	sqlStmt="drop table if exists ${patchesTable}New"
-	RunSql $sqlStmt
-	sqlStmt="create table ${patchesTable}New like ${patchesTable}"
-	RunSql $sqlStmt
-
-## Load the data from the transactional table into the new table
-	## Get transactional data
+## Get the data from the transactional table
 	SetFileExpansion 'off'
 	sqlStmt="select * from patchControl"
 	RunSql "$dbFile" $sqlStmt
 	SetFileExpansion
 	for rec in "${resultSet[@]}"; do dataRecs+=("$rec"); done
-	## Insert into warehouse table
+	## Check the data
+	for rec in "${resultSet[@]}"; do
+		recType="$(cut -f4 -d'|' <<< "$rec")"
+		if [[ $recType == 'currentRelease' ]]; then
+			## Check the value specified master the master repo
+			product="$(cut -f2 -d'|' <<< "$rec")"
+			[[ $product == 'cat' ]] && product='courseleaf'
+			option="$(cut -f7 -d'|' <<< "$rec")"
+			if [[ $option != 'master' ]]; then
+				Pushd "$gitRepoRoot/${product}.git"
+				tags="$(ProtectedCall "git tag" | tr '\n' ' ')"
+				## Loop through the tags to make sure the specified value is correct
+				found=false
+				for tag in $tags; do
+					[[ $tag == $option ]] && { found=true; break; }
+				done
+				if [[ $found != true ]]; then
+					Terminate "Specified value for 'currentRelease' ($option) in the transactional database is not a valid git tag\n\t\t$rec"
+				fi
+			fi
+		fi
+	done
+
+## Data is good, Make a copy of the warehouse table 
+	sqlStmt="drop table if exists ${patchesTable}New"
+	RunSql $sqlStmt
+	sqlStmt="create table ${patchesTable}New like ${patchesTable}"
+	RunSql $sqlStmt
+
+## Insert into warehouse table
 	for ((i=0; i<${#dataRecs[@]}; i++)); do
 
 		sqlStmt="insert into ${patchesTable}New ($fields) values("
@@ -122,3 +144,4 @@ Goodbye 0 #'alert'
 ## 06-18-2018 @ 10:49:14 - 1.0.-1 - dscudiero - Allow passing in a userid name to update
 ## 06-18-2018 @ 15:19:04 - 1.0.-1 - dscudiero - Initial load
 ## 06-18-2018 @ 15:32:23 - 1.0.-1 - dscudiero - Change the name of the transactional table
+## 07-02-2018 @ 13:32:59 - 1.0.-1 - dscudiero - Add data checks for the 'currentRecord' records
