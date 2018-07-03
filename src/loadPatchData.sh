02436ce9c987425c6693f23119b4b2a8fb476c29
David Scudiero <dscudiero@leepfrog.com>
2018-07-03 07:04:00 -0500

    (1.0.-1) Add debug statements

--- src/loadPatchData.sh
+++ src/loadPatchData.sh
@@ -88,19 +88,25 @@ fi
 	for rec in "${dataRecs[@]}"; do
 		recType="$(cut -f4 -d'|' <<< "$rec")"
 		if [[ $recType == 'currentRelease' ]]; then
+dump 1 rec
 			## Check the value specified master the master repo
 			product="$(cut -f2 -d'|' <<< "$rec")"
+dump 1 -t product
 			[[ $product == 'cat' ]] && product='courseleaf'
 			option="$(cut -f7 -d'|' <<< "$rec")"
+dump 1 -t option
 			if [[ $option != 'master' ]]; then
 				Pushd "$gitRepoRoot/${product}.git"
 				tags="$(ProtectedCall "git tag" | tr '\n' ' ')"
+dump 1 -t tags
 				Popd
 				## Loop through the tags to make sure the specified value is correct
 				found=false
 				for tag in $tags; do
+dump 1 -t2 tag
 					[[ $tag == $option ]] && { found=true; break; }
 				done
+dump 1 -t2 found
 				[[ $found != true ]] && Terminate "Specified value for 'currentRelease' ($option) in the transactional database is not a valid git tag\n\t\t$rec"
 			fi
 		fi
@@ -147,3 +153,4 @@ Goodbye 0 #'alert'
 ## 07-02-2018 @ 14:09:18 - 1.0.-1 - dscudiero - Tweak messaging
 ## 07-02-2018 @ 15:04:46 - 1.0.-1 - dscudiero - Added debug statements
 ## 07-02-2018 @ 16:18:12 - 1.0.-1 - dscudiero - Remove debug statements
+## 07-03-2018 @ 07:04:00 - 1.0.-1 - dscudiero - Add debug statements
