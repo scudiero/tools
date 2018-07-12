ecd6af336d06cf56248cf73256a40fe6c2511ff5
David Scudiero <dscudiero@leepfrog.com>
2018-07-12 09:35:52 -0500

    (1.0.2) If no backupDir was specified then make sure we do not pass backup options to rsync

--- lib/RsyncCopy.sh
+++ lib/RsyncCopy.sh
@@ -1,6 +1,6 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="1.0.1" # -- dscudiero -- Thu 07/12/2018 @ 08:58:11
+# version="1.0.2" # -- dscudiero -- Thu 07/12/2018 @ 09:31:57
 #===================================================================================================
 #===================================================================================================================
 # Use rsync to sychronize two directories
@@ -17,7 +17,7 @@ function RsyncCopy {
 	local source="$1"; shift || true
 	local target="$1"; shift || true
 	local ignoreList="${1:-none}"; shift || true; [[ $ignoreList == 'none' ]] && unset ignoreList
-	local backupDir="${1:-/dev/null}"
+	local backupDir="$1"
 	local token rsyncOpts source target rsyncOut rsyncFilters rsyncVerbose rc
 	dump source target ignoreList backupDir | Indent | Indent >> "$logFile"
 
@@ -30,13 +30,14 @@ function RsyncCopy {
 	[[ -f $rsyncErr ]] && rm -f "$rsyncErr"
 	[[ -f $tmpFile ]] && rm -f "$tmpFile"
 
-	[[ $backupDir != '/dev/null' && ! -d $backupDir ]] && $DOIT mkdir -p $backupDir
+	[[ -n $backupDir && ! -d $backupDir ]] && $DOIT mkdir -p $backupDir
 
 	## Set rsync options
-		rsyncOpts='-rptcmlb' ## r=recursive, p=permissions, t=times, -c=checksums, m=ignoreImptyDirs, l=links, b=backup
+		rsyncOpts='-rptcml' ## r=recursive, p=permissions, t=times, -c=checksums, m=ignoreImptyDirs, l=links
 		[[ $quiet != true ]] && rsyncOpts="${rsyncOpts}v"
+		[[ -n $backupDir ]] && rsyncOpts="${rsyncOpts}b --backup-dir=$backupDir"
 		[[ $informationOnlyMode == true || -n $DOIT ]] && rsyncOpts="${rsyncOpts}n" # n=dryRun
-		rsyncOpts="$rsyncOpts --backup-dir=$backupDir --include-from=$rsyncFilters"
+		rsyncOpts="$rsyncOpts --include-from=$rsyncFilters"
 		dump rsyncOpts | Indent | Indent >> "$logFile"
 
 	## Build the filters file
@@ -91,3 +92,4 @@ export -f RsyncCopy
 ## 06-05-2018 @ 15:23:25 - 1.0.0 - dscudiero - Tweaked rsync call amd default include statements
 ## 06-05-2018 @ 16:06:13 - 1.0.0 - dscudiero - Cosmetic/minor change/Sync
 ## 07-12-2018 @ 08:59:00 - 1.0.1 - dscudiero - Fix problem where no backup directory was specified and we were checing to see if the backupDir existed.
+## 07-12-2018 @ 09:35:52 - 1.0.2 - dscudiero - If no backupDir was specified then make sure we do not pass backup options to rsync
