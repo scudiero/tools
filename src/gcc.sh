9cb9271325cd5085b08a41589e1cc30ec53feeb3
David Scudiero <dscudiero@leepfrog.com>
2018-11-13 13:51:58 -0600

    Cosmetic/minor change/Sync

--- src/gcc.sh
+++ src/gcc.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
-#=========================================================================================================================================================================
+#=======================================================================================================================
 module=$1; shift
 [[ -z $module ]] && echo "Sorry, you must specify a module name." && exit
 
 compiler='/usr/bin/g++'
-cppSrc="$HOME/tools/src/c++"
-cppLibs="$HOME/tools/lib/c++"
-libraries=$(ls )
+cppSrc="$HOME/tools/src/cpp"
+cppLibs="$HOME/tools/lib/cpp"
 
-## Set the CLASSPATH
-	ldLibPathSave="$LD_LIBRARY_PATH"
-	unset LD_LIBRARY_PATH libPath libs
+## Set the includes string
+	includedStr="-I /usr/include/mysql"
+	unset libs
 	pushd "$cppLibs" &> /dev/null
 	libs=$(find . -maxdepth 1 -type l -printf ",%f"); #len=${#libs}
-	#for lib in ${libs:0:len-1}; do
 	for lib in ${libs:1}; do
-		libPath="$libPath,$(pwd)/$lib/libs"
+		includedStr="$includedStr -I $(pwd)/$lib"
 	done
-	libPath="${libPath:1}"
-	#echo "libPath = '$libPath"
-	export LD_LIBRARY_PATH="$libPath"
+	# includedStr="${includedStr:1}"
+	popd &> /dev/null
 
 ## Compile the module
 	pushd "$cppSrc" &> /dev/null
-	moduleFile="${module}.cpp"
+	[[ -r ./${module}.cpp ]] && moduleFile="${module}.cpp" || moduleFile="${module}"
 	[[ ! -r $moduleFile ]] && echo -e "Error, could not locate module file '$moduleFile' in '$cppSrc'" && exit -3
-	$compiler -o "../../../bin/$module" $moduleFile ; rc=$?
+
+	# $compiler -o "../../../bin/$module" $includedStr $moduleFile -L /usr/lib64/mysql -l mysqlclient -std=gnu++0x 
+	$compiler -o "../../../bin/$module" $includedStr $moduleFile
+	rc=$?
 	echo -e "\t$module compiled, rc=$rc"
 	popd &> /dev/null
 
-## Cleanup
-	export LD_LIBRARY_PATH="$ldLibPathSave"
-
+## Done
 exit 0
-## 09-25-2017 @ 15.49.50 - dscudiero - General syncing of dev to prod
