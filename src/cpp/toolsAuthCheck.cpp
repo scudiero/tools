8d8e53d2ac00ab6933f78d6d1dcadfb1aa5fc9f3
David Scudiero <dscudiero@leepfrog.com>
2018-11-30 09:47:27 -0600

    (1.0.3) Cosmetic/minor change/Sync

--- src/cpp/toolsAuthCheck.cpp
+++ src/cpp/toolsAuthCheck.cpp
@@ -1,7 +1,7 @@
 //==================================================================================================
 // XO NOT AUTOVERSION
 //==================================================================================================
-// version="1.0.2" // -- dscudiero -- Fri 11/30/2018 @ 09:46:33
+// version="1.0.3" // -- dscudiero -- Fri 11/30/2018 @ 09:47:17
 //==================================================================================================
 // tools -- Check if the user is authorized to run a particular script
 // Usage toolsAuthCheck scriptName <options>
@@ -233,3 +233,4 @@ int main(int argc, char *argv[], char **envVarPtr) {
 } // main
 // 11-30-2018 @ 09:45:36 - 1.0.1 - dscudiero - Testing
 // 11-30-2018 @ 09:46:46 - 1.0.2 - dscudiero - Cosmetic/minor change/Sync
+// 11-30-2018 @ 09:47:27 - 1.0.3 - dscudiero - Cosmetic/minor change/Sync
