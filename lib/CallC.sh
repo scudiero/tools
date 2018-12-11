12dd05776f77e0d6125f871abc28a6c2617a12a5
David Scudiero <dscudiero@leepfrog.com>
2018-12-11 08:49:56 -0600

    (1.0.38) Add local tp variable declaraions, turn off error trapping

--- lib/CallC.sh
+++ lib/CallC.sh
@@ -1,23 +1,19 @@
 ## XO NOT AUTOVERSION
 #===================================================================================================
-# version="1.0.14" # -- dscudiero -- Fri 12/07/2018 @ 14:13:34
+# version="1.0.38" # -- dscudiero -- Mon 12/10/2018 @ 14:31:52
 #===================================================================================================
 #===================================================================================================
 # Copyright 2018 David Scudiero -- all rights reserved.
 # All rights reserved
 #===================================================================================================
 function CallC {
-	module=$1; shift
-	myRhel=$(cat /etc/redhat-release | cut -d" " -f3)
+	local module=$1; shift
+	local myRhel=$(cat /etc/redhat-release | cut -d" " -f3)
 	[[ $myRhel == 'release' ]] && myRhel=$(cat /etc/redhat-release | cut -d" " -f4)
 
-	## Put data into the shell env pool to make it avaiable to the called pgm
-	exportVars="verify client env envs srcEnv tgtEnv product products"
-	for var in $exportVars; do
-			[[ -n ${!var} ]] && export $var="${!var}"
-	done
-
 	## Call pgm, check local first
+	local previousTrapERR=$(trap -p ERR | cut -d ' ' -f3-)
+	trap - ERR
 	if [[ -x $HOME/bin/${module}-rhel${myRhel:0:1} && $USELOCAL == true ]]; then
 		eval $HOME/bin/${module}-rhel${myRhel:0:1} $*
 	elif [[ -x $TOOLSPATH/bin/${module}-rhel${myRhel:0:1} ]]; then
@@ -25,16 +21,18 @@ function CallC {
 	else 
 		eval ${module}-rhel${myRhel:0:1} $*
 	fi
-	return $?
+	rc=$?
+	[[ -n $previousTrapERR ]] && eval "trap $previousTrapERR"
+	return $rc
 	
 } #CallC
 export -f CallC
 #===================================================================================================
 # Checkin Log
 #===================================================================================================
-
 ## Wed Jan  4 13:52:47 CST 2017 - dscudiero - General syncing of dev to prod
 ## 12-03-2018 @ 11:18:26 - 1.0.4 - dscudiero - Add full path to the module
 ## 12-06-2018 @ 09:54:36 - 1.0.12 - dscudiero - Update to run module from local bin if found there
 ## 12-07-2018 @ 07:23:33 - 1.0.13 - dscudiero - Check for local bin first
 ## 12-10-2018 @ 10:37:39 - 1.0.14 - dscudiero - Change the way we export data to the program to call
+## 12-11-2018 @ 08:49:56 - 1.0.38 - dscudiero - Add local tp variable declaraions, turn off error trapping
