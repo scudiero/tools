3009427b14b39b2ad99894483b5c6c9c100de27a
David Scudiero <dscudiero@leepfrog.com>
2019-06-07 09:06:28 -0500

    (1.6.30)

--- loader.sh
+++ loader.sh
@@ -537,3 +537,4 @@ function CleanUp {
 ## 04-30-2019 @ 12:07:52 - 1.6.29 - dscudiero -  Do not do auth check for courseleafPatch
 ## 04-30-2019 @ 12:33:58 - 1.6.30 - dscudiero - 
 ## 05-24-2019 @ 14:00:04 - 1.6.30 - dscudiero - 
+## 06-07-2019 @ 09:06:27 - 1.6.30 - dscudiero - 
