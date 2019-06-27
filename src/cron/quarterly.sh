71ff2ddec443d79060b88acff106103c61ec8c60
David Scudiero <dscudiero@leepfrog.com>
2019-06-27 08:22:10 -0500

    (2.2.3)  Fix syntax error

--- src/cron/quarterly.sh
+++ src/cron/quarterly.sh
@@ -1,7 +1,7 @@
 #=======================================================================================================================
 # XO NOT AUTOVERSION
 #=======================================================================================================================
-version="2.2.2" # -- dscudiero -- Tue 06/25/2019 @ 08:46:26
+version="2.2.3" # -- dscudiero -- Thu 06/27/2019 @ 08:20:04
 #=======================================================================================================================
 # Run every day at noon from cron
 #=======================================================================================================================
@@ -61,7 +61,7 @@ case "$hostName" in
 	build7)
 		## Run programs/functions
 			pgms=("\"escrowSites illinois -password illinois -outDir $courseleafEscrowedSitesDir -emaliList $escrowEmailAddrs\"")
-			pgms+=("\"escrowSites uis -password uis -outDir $courseleafEscrowedSitesDir -emaliList $escrowEmailAddrs\""))
+			pgms+=("\"escrowSites uis -password uis -outDir $courseleafEscrowedSitesDir -emaliList $escrowEmailAddrs\"")
 			pgms+=("\"escrowSites uic -password uic -outDir $courseleafEscrowedSitesDir -emaliList $escrowEmailAddrs\"")
 			pgms+=("\"escrowSites ewu -password ewu -outDir $courseleafEscrowedSitesDir -emaliList $escrowEmailAddrs\"")
 
@@ -103,3 +103,4 @@ return 0
 ## 01-24-2019 @ 12:50:22 - 2.1.53 - dscudiero - Add encryption code
 ## 06-18-2019 @ 07:14:32 - 2.2.1 - dscudiero - Cosmetic / Miscellaneous cleanup / Sync
 ## 06-25-2019 @ 08:49:23 - 2.2.2 - dscudiero -  Updated how escrowSites is called
+## 06-27-2019 @ 08:20:27 - 2.2.3 - dscudiero -  Fix syntax error
